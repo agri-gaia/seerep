ce2182ea52dbb8b9bef784bdc49016a0f8ab26d3
Sebastian Pütz <spuetz@uos.de>
2021-03-04 20:09:41 +0100

    add conversions for PointField and PointCloud2

--- include/agri_gaia_ros/conversions.h
+++ include/agri_gaia_ros/conversions.h
@@ -4,13 +4,62 @@
 #include <std_msgs/Header.h>
 #include "Header.pb.h"
 
+#include <sensor_msgs/PointField.h>
+#include "PointField.pb.h"
+
+#include <sensor_msgs/PointCloud2.h>
+#include "PointCloud2.pb.h"
 
 namespace agri_gaia_ros
 {
 
-  ag::ros::Header toProto(std_msgs::Header& header);
+  /**
+   * @brief Converts a ROS std_msgs/Header message to the corresponding
+   * Protobuf Header message
+   * @param header std_msgs/Header
+   * @return Protobuf Header message
+   */
+  ag::ros::Header toProto(const std_msgs::Header& header);
 
+  /**
+   * @brief Converts a Protobuf Header message to the corresponding
+   * ROS std_msgs/Header message
+   * @param header Protobuf Header
+   * @return ROS std_msgs/Header
+   */
+  std_msgs::Header toROS(const ag::ros::Header& header);
 
+  /**
+   * @brief Converts a ROS sensor_msgs/PointField message to the corresponding
+   * Protobuf PointField message
+   * @param point_field sensor_msgs/PointField
+   * @return Protobuf PointField message
+   */
+  ag::ros::PointField toProto(const sensor_msgs::PointField& point_field);
+
+  /**
+   * @brief Converts a Protobuf PointField message to the corresponding
+   * ROS sensor_msgs/PointField message
+   * @param point_field Protobuf PointField
+   * @return ROS std_sensor_msgs/PointField
+   */
+  sensor_msgs::PointField toROS(const ag::ros::PointField& point_field);
+
+  /**
+   * @brief Converts a ROS sensor_msgs/PointCloud2 message to the corresponding
+   * Protobuf PointCloud2 message
+   * @param point_field sensor_msgs/PointCloud2
+   * @return Protobuf PointCloud2 message
+   */
+  ag::ros::PointCloud2 toProto(const sensor_msgs::PointCloud2& cloud);
+
+  /**
+   * @brief Converts a Protobuf PointCloud2 message to the corresponding
+   * ROS sensor_msgs/PointCloud2 message
+   * @param cloud Protobuf PointCloud2
+   * @return ROS std_sensor_msgs/PointCloud2
+   */
+  sensor_msgs::PointCloud2 toROS(const ag::ros::PointCloud2& cloud);
 
 } /* namespace agri_gaia_ros */
 
--- src/agri_gaia_ros/conversions.cpp
+++ src/agri_gaia_ros/conversions.cpp
@@ -2,8 +2,10 @@
 
 namespace agri_gaia_ros
 {
-
-  ag::ros::Header toProto(std_msgs::Header& header)
+  /*
+   * Header
+   */
+  ag::ros::Header toProto(const std_msgs::Header& header)
   {
     ag::ros::Header ret;
     ret.set_seq(header.seq);
@@ -12,5 +14,74 @@ namespace agri_gaia_ros
     ret.mutable_stamp()->set_nanos(header.stamp.nsec);
     return ret;
   }
+
+  std_msgs::Header toROS(const ag::ros::Header& header)
+  {
+    std_msgs::Header ret;
+    ret.seq = header.seq();
+    ret.frame_id = header.frame_id();
+    ret.stamp.sec = header.stamp().seconds();
+    ret.stamp.nsec = header.stamp().nanos();
+    return ret;
+  }
+
+  /*
+   * PointField
+   */
+  ag::ros::PointField toProto(const sensor_msgs::PointField& point_field)
+  {
+    ag::ros::PointField ret;
+    ret.set_name(point_field.name);
+    ret.set_offset(point_field.offset);
+    ret.set_datatype(ag::ros::PointField_Datatype(point_field.datatype));
+    ret.set_count(point_field.count);
+    return ret;
+  }
+
+  sensor_msgs::PointField toROS(const ag::ros::PointField& point_field)
+  {
+    sensor_msgs::PointField ret;
+    ret.name = point_field.name();
+    ret.offset = point_field.offset();
+    ret.datatype = point_field.datatype();
+    ret.count = point_field.count();
+    return ret;
+  }
+
+  /*
+   * PointCloud2
+   */
+  ag::ros::PointCloud2 toProto(const sensor_msgs::PointCloud2& cloud)
+  {
+    ag::ros::PointCloud2 ret;
+    *ret.mutable_header() = toProto(cloud.header);
+    ret.set_height(cloud.height);
+    ret.set_width(cloud.width);
+    for(auto field : cloud.fields)
+      *ret.add_fields() = toProto(field);
+    ret.set_is_bigendian(cloud.is_bigendian);
+    ret.set_point_step(cloud.point_step);
+    ret.set_row_step(cloud.row_step);
+    ret.set_data(&cloud.data.front(), cloud.data.size());
+    ret.set_is_dense(cloud.is_dense);
+    return ret;
+  }
+
+  sensor_msgs::PointCloud2 toROS(const ag::ros::PointCloud2& cloud)
+  {
+    sensor_msgs::PointCloud2 ret;
+    ret.header = toROS(cloud.header());
+    ret.height = cloud.height();
+    ret.width = cloud.width();
+    for(auto field: cloud.fields())
+      ret.fields.push_back(toROS(field));
+    ret.is_bigendian = cloud.is_bigendian();
+    ret.point_step = cloud.point_step();
+    ret.row_step = cloud.row_step();
+    std::copy(cloud.data().begin(), cloud.data().end(), std::back_inserter(ret.data));
+    ret.is_dense = cloud.is_dense();
+    return ret;
+  }
+
 
 } /* namespace agri_gaia_ros */
