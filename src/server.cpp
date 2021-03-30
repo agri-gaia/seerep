352875e08e6c33b77c0015d09e35fb356cb91adb
Sebastian Pütz <spuetz@uos.de>
2021-03-30 12:17:24 +0200

    initial commit

---
+++ src/server.cpp
@@ -0,0 +1,24 @@
+#include "ag_grpc_hdf5/server.h"
+
+// grpc
+#include <grpc/grpc.h>
+#include <grpcpp/server.h>
+#include <grpcpp/server_builder.h>
+#include <grpcpp/server_context.h>
+#include <grpcpp/security/server_credentials.h>
+
+// ag
+#include <ag_proto_msgs/transfer_sensor_msgs.grpc.pb.h>
+#include <ag_proto_hdf5/io.h>
+
+namespace ag_grpc_hdf5
+{
+
+} /* namespace ag_grpc_hdf5 */
+
+
+int main(int argc, char** argv)
+{
+
+  return EXIT_SUCCESS;
+}
