ab7110bfdac84f3bb92afeaf692b800de1bdb273
Mark Höllmann <mark.hoellmann@dfki.de>
2021-06-03 18:21:00 +0200

    add seerep-core package

---
+++ seerep-srv/seerep-core/include/seerep-core/bounding-box-indexer.h
@@ -0,0 +1,21 @@
+#ifndef SEEREP_CORE_BB_INDEXER_H_
+#define SEEREP_CORE_BB_INDEXER_H_
+
+#include <functional>
+#include <optional>
+
+namespace seerep_core
+{
+class BoundingBoxIndexer {
+public:
+    BoundingBoxIndexer();
+    virtual 
+
+private:
+
+};
+
+
+} /* namespace seerep_core */
+
+#endif // SEEREP_SERVER_SEEREP_CORE_BB_INDEXER_H_HDF5_CLIENT_H_
