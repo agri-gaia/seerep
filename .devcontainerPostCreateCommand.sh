2f3d965473dee846787398e85abae6e92c6dca00
Mark Höllmann <mark.hoellmann@dfki.de>
2021-06-21 16:57:32 +0000

    add devcontainer

---
+++ .devcontainerPostCreateCommand.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+
+#copy vs code config to root
+cp -r /home/docker/workspace/src/.vscode /home/docker/workspace/.vscode
+
+#build the workspace with catkin
+catkin config  --workspace /home/docker/workspace --cmake-args -DCMAKE_EXPORT_COMPILE_COMMANDS=ON
+catkin build --workspace /home/docker/workspace
+
+#install pre-commit in the git repo
+cd /home/docker/workspace/src || exit 1
+pre-commit install
