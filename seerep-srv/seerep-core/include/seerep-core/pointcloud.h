e819384c17abfc15b04ce8c6bedd8bc748523254
Mark Höllmann <mark.hoellmann@dfki.de>
2021-07-23 15:14:14 +0000

    WIP add pcl for pointcloud handling

--- seerep-srv/seerep-core/include/seerep-core/pointcloud.h
+++ seerep-srv/seerep-core/include/seerep-core/pointcloud.h
@@ -4,10 +4,21 @@
 #include <functional>
 #include <optional>
 
+// seerep-msgs
 #include <seerep-msgs/boundingbox.pb.h>
 #include <seerep-msgs/point_cloud_2.pb.h>
-
+// seerep-hdf5
 #include <seerep-hdf5/io.h>
+// seerep-conversion
+#include <seerep_ros_conversions/conversions.h>
+
+// pcl
+#include <pcl/point_cloud.h>
+#include <pcl/point_types.h>
+#include <pcl/PCLPointCloud2.h>
+#include <pcl/conversions.h>
+
+#include <pcl_conversions/pcl_conversions.h>
 
 namespace seerep_core
 {
