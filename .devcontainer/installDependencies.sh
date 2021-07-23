1eb485a7273c5f7ce7356f6457b30cc24898991b
Mark Höllmann <mark.hoellmann@dfki.de>
2021-07-23 14:15:54 +0000

    fix cmake install

--- .devcontainer/installDependencies.sh
+++ .devcontainer/installDependencies.sh
@@ -34,11 +34,9 @@ pip3 install pre-commit
 echo '####################################################'
 echo '#Install cmake'
 echo '####################################################'
-apt-get update && apt-get install -qy apt-transport-https ca-certificates gnupg software-properties-common wget
-wget -O - https://apt.kitware.com/keys/kitware-archive-latest.asc 2>/dev/null | gpg --dearmor - | tee /etc/apt/trusted.gpg.d/kitware.gpg >/dev/null
-apt-add-repository 'deb https://apt.kitware.com/ubuntu/ focal main'
-apt-get update && apt-get install -qy kitware-archive-keyring && rm /etc/apt/trusted.gpg.d/kitware.gpg
-apt-get update && apt-get install -qy cmake
+bash <(curl -s https://apt.kitware.com/kitware-archive.sh)
+apt-get update -q && apt-get install -qy cmake
+
 
 
 echo '####################################################'
