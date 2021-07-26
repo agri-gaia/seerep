9a677051a1e0afb988843f8ec9470d6f2b4f2255
Mark Höllmann <mark.hoellmann@dfki.de>
2021-07-26 15:50:13 +0000

    WIP: spatial query for PC added (untested)

--- seerep-srv/seerep-core/src/pointcloud.cpp
+++ seerep-srv/seerep-core/src/pointcloud.cpp
@@ -11,59 +11,69 @@ Pointcloud::~Pointcloud()
 std::optional<seerep::PointCloud2> Pointcloud::getData(seerep_hdf5::SeerepHDF5IO& hdf5_io, const std::string& id,
                                                        const seerep::Boundingbox bb)
 {
-  // Pointcloud::BoundingBox BoundingBox = getBoundingBox(bb);
+  Eigen::Vector4f minPt, maxPt;
+  getBoundingBox(minPt, maxPt, bb);
   std::optional<seerep::PointCloud2> pc = hdf5_io.readPointCloud2(id);
 
   if (pc)
   {
-    // sensor_msgs::PointCloud2 pc_ros = seerep_ros_conversions::toROS(pc.value());
+    sensor_msgs::PointCloud2 pc_ros = seerep_ros_conversions::toROS(pc.value());
 
-    // pcl::PCLPointCloud2 pcl_pc2;
-    // pcl_conversions::toPCL(pc_ros, pcl_pc2);
-    // pcl::PointCloud<pcl::PointXYZ>::Ptr temp_cloud(new pcl::PointCloud<pcl::PointXYZ>);
-    // pcl::fromPCLPointCloud2(pcl_pc2, *temp_cloud);
+    pcl::PCLPointCloud2 pcl_pc2;
+    pcl_conversions::toPCL(pc_ros, pcl_pc2);
+    pcl::PointCloud<pcl::PointXYZ>::Ptr temp_cloud(new pcl::PointCloud<pcl::PointXYZ>);
+    pcl::fromPCLPointCloud2(pcl_pc2, *temp_cloud);
 
-    return pc;
+    pcl::CropBox<pcl::PointXYZ> boxFilter;
+    boxFilter.setMin(minPt);
+    boxFilter.setMax(maxPt);
+    boxFilter.setInputCloud(temp_cloud);
+    pcl::PointCloud<pcl::PointXYZ>::Ptr filtered_cloud(new pcl::PointCloud<pcl::PointXYZ>);
+    boxFilter.filter(*filtered_cloud);
+
+    pcl::toPCLPointCloud2(*filtered_cloud, pcl_pc2);
+    sensor_msgs::PointCloud2 pc2_msg;
+    pcl_conversions::fromPCL(pcl_pc2, pc2_msg);
+    return seerep_ros_conversions::toProto(pc2_msg);
   }
 
   return std::nullopt;
 }
 
-Pointcloud::BoundingBox Pointcloud::getBoundingBox(const seerep::Boundingbox bb)
+void Pointcloud::getBoundingBox(Eigen::Vector4f& minPt, Eigen::Vector4f& maxPt, const seerep::Boundingbox& bb)
 {
-  Pointcloud::BoundingBox boundingbox;
-
   if (bb.point_max().x() < bb.point_min().x())
   {
-    boundingbox.xmax = bb.point_min().x();
-    boundingbox.xmin = bb.point_max().x();
+    maxPt(0) = bb.point_min().x();
+    minPt(0) = bb.point_max().x();
   }
   else
   {
-    boundingbox.xmax = bb.point_max().x();
-    boundingbox.xmin = bb.point_min().x();
+    maxPt(0) = bb.point_max().x();
+    minPt(0) = bb.point_min().x();
   }
   if (bb.point_max().y() < bb.point_min().y())
   {
-    boundingbox.xmax = bb.point_min().y();
-    boundingbox.xmin = bb.point_max().y();
+    maxPt(1) = bb.point_min().y();
+    minPt(1) = bb.point_max().y();
   }
   else
   {
-    boundingbox.xmax = bb.point_max().y();
-    boundingbox.xmin = bb.point_min().y();
+    maxPt(1) = bb.point_max().y();
+    minPt(1) = bb.point_min().y();
   }
   if (bb.point_max().z() < bb.point_min().z())
   {
-    boundingbox.xmax = bb.point_min().z();
-    boundingbox.xmin = bb.point_max().z();
+    maxPt(2) = bb.point_min().z();
+    minPt(2) = bb.point_max().z();
   }
   else
   {
-    boundingbox.xmax = bb.point_max().z();
-    boundingbox.xmin = bb.point_min().z();
+    maxPt(2) = bb.point_max().z();
+    minPt(2) = bb.point_min().z();
   }
 
-  return boundingbox;
+  maxPt(3) = 1.0f;
+  minPt(3) = 1.0f;
 }
 } /* namespace seerep_core */
