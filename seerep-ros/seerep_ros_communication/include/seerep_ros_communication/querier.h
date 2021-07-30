84d903ec6e4bb66f32f6e3dc2d449254fd4ca323
Mark Höllmann <mark.hoellmann@dfki.de>
2021-07-30 13:36:06 +0000

    added querier

---
+++ seerep-ros/seerep_ros_communication/include/seerep_ros_communication/querier.h
@@ -0,0 +1,40 @@
+#ifndef SEEREP_GRPC_ROS_QUERIER_H_
+#define SEEREP_GRPC_ROS_QUERIER_H_
+
+#include <functional>
+#include <optional>
+
+// grpc
+#include <grpc/grpc.h>
+#include <grpcpp/create_channel.h>
+#include <grpcpp/client_context.h>
+#include <grpcpp/security/credentials.h>
+
+// ag
+#include <seerep-com/query_data.grpc.pb.h>
+#include <seerep_ros_conversions/conversions.h>
+
+// ros
+#include <ros/ros.h>
+#include <ros/master.h>
+
+// pkg
+#include "types.h"
+
+namespace seerep_grpc_ros
+{
+class QueryData
+{
+public:
+  QueryData(std::shared_ptr<grpc::Channel> channel_ptr);
+
+  void queryPointcloud(const seerep::Boundingbox& bb, sensor_msgs::PointCloud2& pc2) const;
+
+private:
+  StubPtr stub_;
+  ros::NodeHandle nh;
+};
+
+} /* namespace seerep_grpc_ros */
+
+#endif  // SEEREP_GRPC_ROS_QUERIER_H_
