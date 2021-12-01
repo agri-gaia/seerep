b4d82abc13a004240f853cbac3ea2cb15bc8c909
Mark Höllmann <mark.hoellmann@dfki.de>
2021-12-01 18:54:53 +0000

    add util script using gRPC in Python (project creation as example)

---
+++ util/sendImageViaGRPC.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+import sys
+
+# sys.path.insert(1, "/home/docker/workspace/build/seerep-msgs")
+# sys.path.insert(1, "/home/docker/workspace/build/seerep-com")
+
+import transfer_sensor_msgs_pb2_grpc as transferMsgs
+import grpc
+import projectCreation_pb2
+import projectCreated_pb2
+
+channel = grpc.insecure_channel("localhost:9090")
+
+stub = transferMsgs.TransferSensorMsgsStub(channel)
+
+response = stub.CreateProject(projectCreation_pb2.ProjectCreation(name="testprojectpython"))
+
+
+print(response.uuid)
