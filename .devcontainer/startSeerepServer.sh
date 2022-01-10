5e60f20810f79e903c1f4b1c8bb733d8c46692dc
Mark Höllmann <mark.hoellmann@dfki.de>
2022-01-10 08:40:45 +0000

    fix shellcheck error in pre-commit checks

--- .devcontainer/startSeerepServer.sh
+++ .devcontainer/startSeerepServer.sh
@@ -9,5 +9,8 @@ DIR="$( cd "$( dirname "$0" )" && pwd )"
 catkin build --workspace /home/docker/workspace  || exit
 
 #run the server
+
+# the next line is needed to avoid https://github.com/koalaman/shellcheck/wiki/SC1091 if catkin build hasn't run yet
+# shellcheck source=/dev/null
 source /home/docker/workspace/devel/setup.bash
 "$DIR"/../../devel/bin/seerep-server_server /home/docker/workspace/seerep-data || exit
