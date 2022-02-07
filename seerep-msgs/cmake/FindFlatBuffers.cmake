d0ebd9de6056385fba9a6813f642054552b6ced6
Sebastian Pütz <spuetz@uos.de>
2022-02-07 16:46:43 +0000

    use .fb.h as suffix

--- seerep-msgs/cmake/FindFlatBuffers.cmake
+++ seerep-msgs/cmake/FindFlatBuffers.cmake
@@ -39,12 +39,12 @@ if(FLATBUFFERS_FOUND)
     foreach(FILE ${ARGN})
       get_filename_component(FLATC_OUTPUT ${FILE} NAME_WE)
       set(FLATC_OUTPUT
-        "${CMAKE_CURRENT_BINARY_DIR}/${FLATC_OUTPUT}_generated.h")
+        "${CMAKE_CURRENT_BINARY_DIR}/${FLATC_OUTPUT}.fb.h")
       list(APPEND FLATC_OUTPUTS ${FLATC_OUTPUT})
 
       add_custom_command(OUTPUT ${FLATC_OUTPUT}
         COMMAND ${FLATBUFFERS_FLATC_EXECUTABLE}
-        ARGS -c -o "${CMAKE_CURRENT_BINARY_DIR}/" ${FILE}
+        ARGS --filename-suffix ".fb" -c -o "${CMAKE_CURRENT_BINARY_DIR}/" ${FILE}
         DEPENDS ${FILE}
         COMMENT "Building C++ header for ${FILE}"
         WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR})
