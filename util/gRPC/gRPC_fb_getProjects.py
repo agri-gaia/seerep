38f69614a82aa1c44574eb054120182e798d79e1
Mark Höllmann <mark.hoellmann@dfki.de>
2022-03-23 14:58:53 +0000

    add pb to protobuf util script names

---
+++ util/gRPC/gRPC_fb_getProjects.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+# import sys
+
+# import Empty
+# import meta_operations_pb2_grpc as metaOperations
+# import grpc
+
+# server = "localhost:9090"
+# channel = grpc.insecure_channel(server)
+
+# stub = metaOperations.MetaOperationsStub(channel)
+
+# response = stub.GetProjects(empty_pb2.Empty())
+
+
+# print(server + " has the following projects (name/uuid):")
+# for projectinfo in response.projects:
+#     print("\t" + projectinfo.name + " " + projectinfo.uuid)
