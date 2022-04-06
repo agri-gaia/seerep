cddbd5d47c9bc27ba3f5c310795deb5a21e695c7
lenaherrmann-dfki <lena.herrmann@dfki.de>
2022-04-06 16:46:39 +0200

    clean up docker-container and add README

--- docker/start_server.sh
+++ docker/start_server.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 # shellcheck disable=SC1091
 
-echo "arguments"
 echo "$@"
 
 source /opt/ros/noetic/setup.bash
-
 source /home/docker/seerep_ws/devel/setup.bash
 
 /home/docker/seerep_ws/devel/bin/seerep-server_server "$@"
