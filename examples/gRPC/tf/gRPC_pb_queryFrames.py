1a645e267726a5135e0c0b1ccf38016e3616d128
Mark Höllmann <mark.hoellmann@dfki.de>
2022-08-08 17:03:05 +0000

    update server URI to new argoCD deployment

--- examples/gRPC/tf/gRPC_pb_queryFrames.py
+++ examples/gRPC/tf/gRPC_pb_queryFrames.py
@@ -4,23 +4,16 @@ import os
 import sys
 
 import frame_query_pb2 as frameQuery
-import grpc
 import meta_operations_pb2_grpc as metaOperations
 import tf_service_pb2_grpc as tfService
 from google.protobuf import empty_pb2
 
-# import numpy as np
+script_dir = os.path.dirname(__file__)
+util_dir = os.path.join(script_dir, '..')
+sys.path.append(util_dir)
+import util
 
-# # server with certs
-# __location__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
-# with open(os.path.join(__location__, '../tls.pem'), 'rb') as f:
-#     root_cert = f.read()
-# server = "seerep.robot.10.249.3.13.nip.io:32141"
-# creds = grpc.ssl_channel_credentials(root_cert)
-# channel = grpc.secure_channel(server, creds)
-
-# server without certs
-channel = grpc.insecure_channel("localhost:9090")
+channel = util.get_gRPC_channel()
 
 stub = tfService.TfServiceStub(channel)
 stubMeta = metaOperations.MetaOperationsStub(channel)
---
+++ examples/gRPC/util.py
@@ -0,0 +1,26 @@
+import os
+
+import grpc
+
+
+def get_gRPC_channel(target="prod"):
+    if target == "prod" or target == "dev":
+        if target == "prod":
+            targetName = "seerep-prod"
+        else:
+            targetName = "seerep-dev"
+
+        # server with certs
+        __location__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
+        with open(os.path.join(__location__, '../../certs/tls.pem'), 'rb') as f:
+            root_cert = f.read()
+        server = "seerep." + targetName + ".10.249.3.13.nip.io:32141"
+        creds = grpc.ssl_channel_credentials(root_cert)
+        channel = grpc.secure_channel(server, creds)
+
+    elif target == "local":
+        # server without certs
+        server = "localhost:9090"
+        channel = grpc.insecure_channel(server)
+
+    return channel
