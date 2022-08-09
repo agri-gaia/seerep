1be93be76afd0f174b8cbd0da748f76ae1064349
Mark Höllmann <mark.hoellmann@dfki.de>
2022-08-09 17:11:51 +0000

    update URI and certs

--- examples/gRPC/util.py
+++ examples/gRPC/util.py
@@ -6,15 +6,15 @@ import grpc
 def get_gRPC_channel(target="prod"):
     if target == "prod" or target == "dev":
         if target == "prod":
-            targetName = "seerep-prod"
+            targetName = "robot"
         else:
-            targetName = "seerep-dev"
+            targetName = "robot"
 
         # server with certs
         __location__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
         with open(os.path.join(__location__, '../../certs/tls.pem'), 'rb') as f:
             root_cert = f.read()
-        server = "seerep." + targetName + ".10.249.3.13.nip.io:32141"
+        server = "seerep." + targetName + ".10.249.3.13.nip.io:31723"
         creds = grpc.ssl_channel_credentials(root_cert)
         channel = grpc.secure_channel(server, creds)
 
