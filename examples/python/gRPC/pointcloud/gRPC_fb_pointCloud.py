fb45b1d377d83f7dab8811347dd0a478b6cee5c7
Julian Arkenau <julian.arkenau@dfki.de>
2022-09-14 11:57:09 +0000

    refactor: fb point cloud example to use fb util

--- examples/python/gRPC/pointcloud/gRPC_fb_pointCloud.py
+++ examples/python/gRPC/pointcloud/gRPC_fb_pointCloud.py
@@ -6,185 +6,91 @@ import time
 
 import flatbuffers
 import numpy as np
-from fb import (
-    Boundingbox,
-    BoundingBoxLabeled,
-    Header,
-    LabelWithInstance,
-    Point,
-    PointCloud2,
-    PointField,
-    Timestamp,
-)
+from fb import PointCloud2
 from fb import point_cloud_service_grpc_fb as pointCloudService
 
 script_dir = os.path.dirname(__file__)
 util_dir = os.path.join(script_dir, '..')
 sys.path.append(util_dir)
+
 import util
-import util_fb
+from util_fb import (
+    addToBoundingBoxLabeledVector,
+    addToGeneralLabelsVector,
+    addToPointFieldVector,
+    createBoundingBoxes,
+    createBoundingBoxesLabeled,
+    createHeader,
+    createLabelsWithInstance,
+    createPoint,
+    createPointFields,
+    createTimeStamp,
+    getOrCreateProject,
+)
+
+NUM_GENERAL_LABELS = 10
+NUM_BB_LABELS = 10
+NUM_POINT_CLOUDS = 10
 
 
-def createTimeStamp(builder, time):
-    '''Create a flatbuffers time stamp'''
-    Timestamp.Start(builder)
-    Timestamp.AddSeconds(builder, time)
-    Timestamp.AddNanos(builder, 0)
-    return Timestamp.End(builder)
+def createPointCloud(builder, header, height=960, width=1280):
+    '''Creates a flatbuffers point cloud message'''
+    pointFields = createPointFields(builder, "xyz", 7, 4, 1)
+    pointFieldsVector = addToPointFieldVector(builder, pointFields)
 
+    # create general labels
+    labelsGeneral = createLabelsWithInstance(
+        builder,
+        ["GeneralLabel" + str(i) for i in range(NUM_GENERAL_LABELS)],
+        ["InstanceUuid" + str(i) for i in range(NUM_GENERAL_LABELS)],
+    )
+    labelsGeneralVector = addToGeneralLabelsVector(builder, labelsGeneral)
 
-# ToDo add doc for input parameter
-def createPointField(builder, name, offset, datatype, count):
-    '''Creates point field to describe a channel in the point cloud'''
-    nameStr = builder.CreateString(name)
-    PointField.Start(builder)
-    PointField.AddName(builder, nameStr)
-    PointField.AddOffset(builder, offset)
-    PointField.AddDatatype(builder, datatype)
-    PointField.AddCount(builder, count)
-    return PointField.End(builder)
+    # create bounding box labels
+    boundingBoxes = createBoundingBoxes(
+        builder,
+        header,
+        [createPoint(builder, np.random.rand(), np.random.rand(), np.random.rand()) for i in range(NUM_BB_LABELS)],
+        [createPoint(builder, np.random.rand(), np.random.rand(), np.random.rand()) for i in range(NUM_BB_LABELS)],
+    )
+    labelWithInstances = createLabelsWithInstance(
+        builder,
+        ["BoundingBoxLabel" + str(i) for i in range(NUM_BB_LABELS)],
+        ["InstanceUuid" + str(i) for i in range(NUM_BB_LABELS)],
+    )
+    labelsBb = createBoundingBoxesLabeled(builder, labelWithInstances, boundingBoxes)
+    labelsBbVector = addToBoundingBoxLabeledVector(builder, labelsBb)
 
-
-def createPointFields(builder, fields="xyz", datatype=7, count=1):
-    '''Creates fields for all channels of the point cloud'''
-    fieldsList = []
-    offset = 0
-    for field in fields:
-        fieldsList.append(createPointField(builder, field, offset, datatype, count))
-        offset += 4
-    return fieldsList
-
-
-def listToPointFieldVector(builder, pointFieldList):
-    '''Creates fields vector from a list of point fields'''
-    PointCloud2.StartFieldsVector(builder, len(pointFieldList))
-    for pointField in reversed(pointFieldList):
-        builder.PrependUOffsetTRelative(pointField)
-    return builder.EndVector()
-
-
-def createHeader(builder, time, frame, projectUuid):
-    '''Creates a flatbuffers header'''
-    frameIdStr = builder.CreateString(frame)
-    projectUuidStr = builder.CreateString(projectUuid)
-    timeStamp = createTimeStamp(builder, time)
-    Header.Start(builder)
-    Header.AddFrameId(builder, frameIdStr)
-    Header.AddStamp(builder, timeStamp)
-    Header.AddUuidProject(builder, projectUuidStr)
-    return Header.End(builder)
-
-
-def createLabelWithInstance(builder, label="testlabel", instanceUuid="testUuid"):
-    '''Creates a label with an instance uuid'''
-    labelStr = builder.CreateString(label)
-    instanceUuidStr = builder.CreateString(instanceUuid)
-    LabelWithInstance.Start(builder)
-    LabelWithInstance.AddLabel(builder, labelStr)
-    LabelWithInstance.AddInstanceUuid(builder, instanceUuidStr)
-    return LabelWithInstance.End(builder)
-
-
-def createGeneralLabels(builder, numOf=10):
-    '''Creates 'numOf' general labels'''
-    labelsGeneral = []
-    for i in range(numOf):
-        labelsGeneral.append(createLabelWithInstance(builder, "testlabel" + str(i), "testUuid" + str(i)))
-    return labelsGeneral
-
-
-def createPoint(builder, x=np.random.rand(), y=np.random.rand(), z=np.random.rand()):
-    Point.Start(builder)
-    Point.AddX(builder, x)
-    Point.AddY(builder, y)
-    Point.AddZ(builder, z)
-    return Point.End(builder)
-
-
-def createBoundingBox(builder, time, frame, projectUuid):
-    header = createHeader(builder, time, frame, projectUuid)
-    pointMin = createPoint(builder)
-    pointMax = createPoint(builder)
-    Boundingbox.Start(builder)
-    Boundingbox.AddHeader(builder, header)
-    Boundingbox.AddPointMin(builder, pointMin)
-    Boundingbox.AddPointMax(builder, pointMax)
-    return Boundingbox.End(builder)
-
-
-def createBoundingBoxLabeled(builder, labels, instanceUuid, time, frame, projectUuid):
-    labelWithInstance = createLabelWithInstance(builder, labels, instanceUuid)
-    boundingBox = createBoundingBox(builder, time, frame, projectUuid)
-    BoundingBoxLabeled.Start(builder)
-    BoundingBoxLabeled.AddLabelWithInstance(builder, labelWithInstance)
-    BoundingBoxLabeled.AddBoundingBox(builder, boundingBox)
-    return BoundingBoxLabeled.End(builder)
-
-
-def createBoundingBoxesLabeled(builder, projectUuid, numOf=10):
-    boundingBoxLabeled = []
-    for i in range(numOf):
-        boundingBoxLabeled.append(
-            createBoundingBoxLabeled(
-                builder, "testlabel" + str(i), "testUuid" + str(i), int(time.time()), "camera", projectUuid
-            )
-        )
-    return boundingBoxLabeled
-
-
-def listToBoudingBoxLabeledVector(builder, boudingBoxLabeledList):
-    PointCloud2.StartLabelsBbVector(builder, len(boudingBoxLabeledList))
-    for bb in reversed(boudingBoxLabeledList):
-        builder.PrependUOffsetTRelative(bb)
-    return builder.EndVector()
-
-
-def listToGeneralLabelsVector(builder, generalLabelList):
-    '''Creates labels general vector from a list of labels'''
-    PointCloud2.StartLabelsGeneralVector(builder, len(generalLabelList))
-    for label in reversed(generalLabelList):
-        builder.PrependUOffsetTRelative(label)
-    return builder.EndVector()
-
-
-def createPointCloud(builder, header, projectUuid, height=960, width=1280):
-    '''Creates the point cloud message'''
-    pointFields = createPointFields(builder)
-    pointFieldVector = listToPointFieldVector(builder, pointFields)
-
-    labelsGeneral = createGeneralLabels(builder)
-    labelsGeneralVector = listToGeneralLabelsVector(builder, labelsGeneral)
-
-    labelsBb = createBoundingBoxesLabeled(builder, projectUuid)
-    labelsBbVector = listToBoudingBoxLabeledVector(builder, labelsBb)
-
-    # generate ordered point cloud with dim (height, width, 3)
+    # create ordered point cloud with dim (height, width, 3)
     points = np.random.randn(height, width, 3).astype(np.float32)
     pointsVector = builder.CreateByteVector(points.tobytes())
 
+    # add all data into the flatbuffers point cloud message
     PointCloud2.Start(builder)
     PointCloud2.AddHeader(builder, header)
     PointCloud2.AddHeight(builder, points.shape[0])
     PointCloud2.AddWidth(builder, points.shape[1])
     PointCloud2.AddIsBigendian(builder, False)
     PointCloud2.AddPointStep(builder, 12)
-    PointCloud2.AddRowStep(builder, points.shape[1], 12)
-    PointCloud2.AddFields(builder, pointFieldVector)
+    PointCloud2.AddRowStep(builder, points.shape[1] * 12)
+    PointCloud2.AddFields(builder, pointFieldsVector)
     PointCloud2.AddData(builder, pointsVector)
     PointCloud2.AddLabelsGeneral(builder, labelsGeneralVector)
     PointCloud2.AddLabelsBb(builder, labelsBbVector)
     return PointCloud2.End(builder)
 
 
-def createPointClouds(projectUuid, numOf=10):
-    '''Creates 'numOf' point cloud messages'''
+def createPointClouds(projectUuid, numOf):
+    '''Creates numOf pointcloud2 messages as a generator function'''
     theTime = int(time.time())
     for i in range(numOf):
         print(f"Send point cloud: {str(i+1)}")
         builder = flatbuffers.Builder(1024)
 
-        header = createHeader(builder, theTime + i, "camera", projectUuid)
-        pointCloudMsg = createPointCloud(builder, header, projectUuid)
+        timeStamp = createTimeStamp(builder, theTime + i)
+        header = createHeader(builder, timeStamp, "camera", projectUuid)
+
+        pointCloudMsg = createPointCloud(builder, header)
         builder.Finish(pointCloudMsg)
         yield bytes(builder.Output())
 
@@ -192,6 +98,6 @@ def createPointClouds(projectUuid, numOf=10):
 channel = util.get_gRPC_channel()
 stub = pointCloudService.PointCloudServiceStub(channel)
 
-projectUuid = util_fb.get_or_create_project(channel, "testproject", True)
+projectUuid = getOrCreateProject(channel, "testproject", True)
 
-responseBuf = stub.TransferPointCloud2(createPointClouds(projectUuid))
+responseBuf = stub.TransferPointCloud2(createPointClouds(projectUuid, NUM_POINT_CLOUDS))
--- examples/python/gRPC/util_fb.py
+++ examples/python/gRPC/util_fb.py
@@ -1,11 +1,25 @@
 import sys
+import time
 
 import flatbuffers
-from fb import Empty, ProjectCreation, ProjectInfo, ProjectInfos
+from fb import (
+    Boundingbox,
+    BoundingBoxLabeled,
+    Empty,
+    Header,
+    LabelWithInstance,
+    Point,
+    PointCloud2,
+    PointField,
+    ProjectCreation,
+    ProjectInfo,
+    ProjectInfos,
+    Timestamp,
+)
 from fb import meta_operations_grpc_fb as metaOperations
 
 
-def get_or_create_project(channel, name, create=True, mapFrameId="map"):
+def getOrCreateProject(channel, name, create=True, mapFrameId="map"):
     stubMeta = metaOperations.MetaOperationsStub(channel)
 
     builder = flatbuffers.Builder(1024)
@@ -42,3 +56,134 @@ def get_or_create_project(channel, name, create=True, mapFrameId="map"):
             sys.exit()
 
     return projectuuid
+
+
+# Header
+def createTimeStamp(builder, seconds, nanoseconds=0):
+    '''Create a time stamp in flatbuffers'''
+    Timestamp.Start(builder)
+    Timestamp.AddSeconds(builder, seconds)
+    Timestamp.AddNanos(builder, nanoseconds)
+    return Timestamp.End(builder)
+
+
+def createHeader(builder, timeStamp, frame, projectUuid):
+    '''Creates a message header in flatbuffers'''
+    frameStr = builder.CreateString(frame)
+    projectUuidStr = builder.CreateString(projectUuid)
+    Header.Start(builder)
+    Header.AddFrameId(builder, frameStr)
+    Header.AddStamp(builder, timeStamp)
+    Header.AddUuidProject(builder, projectUuidStr)
+    return Header.End(builder)
+
+
+# Point clouds
+def createPointField(builder, name, offset, datatype, count):
+    '''Creates a point field in flatbuffers to describe a channel in the point cloud'''
+    nameStr = builder.CreateString(name)
+    PointField.Start(builder)
+    PointField.AddName(builder, nameStr)
+    PointField.AddOffset(builder, offset)
+    PointField.AddDatatype(builder, datatype)
+    PointField.AddCount(builder, count)
+    return PointField.End(builder)
+
+
+def createPointFields(builder, channels, datatype, dataTypeOffset, count):
+    '''Creates point fields for all specified channels'''
+    pointFieldsList = []
+    offset = 0
+    for channel in channels:
+        pointFieldsList.append(createPointField(builder, channel, offset, datatype, count))
+        offset += dataTypeOffset
+    return pointFieldsList
+
+
+def createLabelWithInstance(builder, label, instanceUuid):
+    '''Creates a label with an associated instance uuid in flatbuffers'''
+    labelStr = builder.CreateString(label)
+    instanceUuidStr = builder.CreateString(instanceUuid)
+    LabelWithInstance.Start(builder)
+    LabelWithInstance.AddLabel(builder, labelStr)
+    LabelWithInstance.AddInstanceUuid(builder, instanceUuidStr)
+    return LabelWithInstance.End(builder)
+
+
+def createLabelsWithInstance(builder, labels, instanceUuids):
+    '''Creates multiple general labels'''
+    assert len(labels) == len(instanceUuids)
+    labelsGeneral = []
+    for label, uuid in zip(labels, instanceUuids):
+        labelsGeneral.append(createLabelWithInstance(builder, label, uuid))
+    return labelsGeneral
+
+
+def createPoint(builder, x, y, z):
+    '''Creates a 3D point in flatbuffers'''
+    Point.Start(builder)
+    Point.AddX(builder, x)
+    Point.AddY(builder, y)
+    Point.AddZ(builder, z)
+    return Point.End(builder)
+
+
+def createBoundingBox(builder, header, pointMin, pointMax):
+    '''Creates a 3D bounding box in flatbuffers'''
+    Boundingbox.Start(builder)
+    Boundingbox.AddHeader(builder, header)
+    Boundingbox.AddPointMin(builder, pointMin)
+    Boundingbox.AddPointMax(builder, pointMax)
+    return Boundingbox.End(builder)
+
+
+def createBoundingBoxes(builder, header, minPoints, maxPoints):
+    assert len(minPoints) == len(maxPoints)
+    boundingBoxes = []
+    for pointMin, pointMax in zip(minPoints, maxPoints):
+        boundingBoxes.append(createBoundingBox(builder, header, pointMin, pointMax))
+    return boundingBoxes
+
+
+def createBoundingBoxLabeled(builder, instance, boundingBox):
+    '''Creates a labeled bounding box in flatbuffers'''
+    BoundingBoxLabeled.Start(builder)
+    BoundingBoxLabeled.AddLabelWithInstance(builder, instance)
+    BoundingBoxLabeled.AddBoundingBox(builder, boundingBox)
+    return BoundingBoxLabeled.End(builder)
+
+
+def createBoundingBoxesLabeled(builder, instances, boundingBoxes):
+    '''Creates multiple labeled bounding boxes'''
+    assert len(instances) == len(boundingBoxes)
+    boundingBoxesLabeled = []
+    for instance, boundingBox in zip(instances, boundingBoxes):
+        boundingBoxesLabeled.append(createBoundingBoxLabeled(builder, instance, boundingBox))
+    return boundingBoxesLabeled
+
+
+def addToBoundingBoxLabeledVector(builder, boundingBoxLabeledList):
+    '''Adds list of boudingBoxLabeled into the labelsBbVector of a flatbuffers pointcloud2'''
+    PointCloud2.StartLabelsBbVector(builder, len(boundingBoxLabeledList))
+    # Note: reverse because we prepend
+    for bb in reversed(boundingBoxLabeledList):
+        builder.PrependUOffsetTRelative(bb)
+    return builder.EndVector()
+
+
+def addToGeneralLabelsVector(builder, generalLabelList):
+    '''Adds list of generalLabels into the labelsGeneralVector of a flatbuffers pointcloud2'''
+    PointCloud2.StartLabelsGeneralVector(builder, len(generalLabelList))
+    # Note: reverse because we prepend
+    for label in reversed(generalLabelList):
+        builder.PrependUOffsetTRelative(label)
+    return builder.EndVector()
+
+
+def addToPointFieldVector(builder, pointFieldList):
+    '''Adds a list of pointFields into the fieldsVector of a flatbuffers pointcloud2'''
+    PointCloud2.StartFieldsVector(builder, len(pointFieldList))
+    # Note: reverse because we prepend
+    for pointField in reversed(pointFieldList):
+        builder.PrependUOffsetTRelative(pointField)
+    return builder.EndVector()
