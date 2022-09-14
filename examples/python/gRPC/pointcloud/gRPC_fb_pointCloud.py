e8b9b580b09e93fd018eaa20749aeb2396339a93
Julian Arkenau <julian.arkenau@dfki.de>
2022-09-14 08:25:25 +0000

    add boudingBoxLabeled into fb point cloud script

--- examples/python/gRPC/pointcloud/gRPC_fb_pointCloud.py
+++ examples/python/gRPC/pointcloud/gRPC_fb_pointCloud.py
@@ -6,7 +6,16 @@ import time
 
 import flatbuffers
 import numpy as np
-from fb import Header, LabelWithInstance, PointCloud2, PointField, Timestamp
+from fb import (
+    Boundingbox,
+    BoundingBoxLabeled,
+    Header,
+    LabelWithInstance,
+    Point,
+    PointCloud2,
+    PointField,
+    Timestamp,
+)
 from fb import point_cloud_service_grpc_fb as pointCloudService
 
 script_dir = os.path.dirname(__file__)
@@ -66,7 +75,7 @@ def createHeader(builder, time, frame, projectUuid):
     return Header.End(builder)
 
 
-def createGeneralLabel(builder, label="testlabel", instanceUuid="testUuid"):
+def createLabelWithInstance(builder, label="testlabel", instanceUuid="testUuid"):
     '''Creates a label with an instance uuid'''
     labelStr = builder.CreateString(label)
     instanceUuidStr = builder.CreateString(instanceUuid)
@@ -80,8 +89,54 @@ def createGeneralLabels(builder, numOf=10):
     '''Creates 'numOf' general labels'''
     labelsGeneral = []
     for i in range(numOf):
-        labelsGeneral.append(createGeneralLabel(builder, "testlabel" + str(i), "testUuid" + str(i)))
+        labelsGeneral.append(createLabelWithInstance(builder, "testlabel" + str(i), "testUuid" + str(i)))
     return labelsGeneral
+
+
+def createPoint(builder, x=np.random.rand(), y=np.random.rand(), z=np.random.rand()):
+    Point.Start(builder)
+    Point.AddX(builder, x)
+    Point.AddY(builder, y)
+    Point.AddZ(builder, z)
+    return Point.End(builder)
+
+
+def createBoundingBox(builder, time, frame, projectUuid):
+    header = createHeader(builder, time, frame, projectUuid)
+    pointMin = createPoint(builder)
+    pointMax = createPoint(builder)
+    Boundingbox.Start(builder)
+    Boundingbox.AddHeader(builder, header)
+    Boundingbox.AddPointMin(builder, pointMin)
+    Boundingbox.AddPointMax(builder, pointMax)
+    return Boundingbox.End(builder)
+
+
+def createBoundingBoxLabeled(builder, labels, instanceUuid, time, frame, projectUuid):
+    labelWithInstance = createLabelWithInstance(builder, labels, instanceUuid)
+    boundingBox = createBoundingBox(builder, time, frame, projectUuid)
+    BoundingBoxLabeled.Start(builder)
+    BoundingBoxLabeled.AddLabelWithInstance(builder, labelWithInstance)
+    BoundingBoxLabeled.AddBoundingBox(builder, boundingBox)
+    return BoundingBoxLabeled.End(builder)
+
+
+def createBoundingBoxesLabeled(builder, projectUuid, numOf=10):
+    boundingBoxLabeled = []
+    for i in range(numOf):
+        boundingBoxLabeled.append(
+            createBoundingBoxLabeled(
+                builder, "testlabel" + str(i), "testUuid" + str(i), int(time.time()), "camera", projectUuid
+            )
+        )
+    return boundingBoxLabeled
+
+
+def listToBoudingBoxLabeledVector(builder, boudingBoxLabeledList):
+    PointCloud2.StartLabelsBbVector(builder, len(boudingBoxLabeledList))
+    for bb in reversed(boudingBoxLabeledList):
+        builder.PrependUOffsetTRelative(bb)
+    return builder.EndVector()
 
 
 def listToGeneralLabelsVector(builder, generalLabelList):
@@ -92,13 +147,16 @@ def listToGeneralLabelsVector(builder, generalLabelList):
     return builder.EndVector()
 
 
-def createPointCloud(builder, header, height=960, width=1280):
+def createPointCloud(builder, header, projectUuid, height=960, width=1280):
     '''Creates the point cloud message'''
     pointFields = createPointFields(builder)
     pointFieldVector = listToPointFieldVector(builder, pointFields)
 
     labelsGeneral = createGeneralLabels(builder)
     labelsGeneralVector = listToGeneralLabelsVector(builder, labelsGeneral)
+
+    labelsBb = createBoundingBoxesLabeled(builder, projectUuid)
+    labelsBbVector = listToBoudingBoxLabeledVector(builder, labelsBb)
 
     # generate ordered point cloud with dim (height, width, 3)
     points = np.random.randn(height, width, 3).astype(np.float32)
@@ -110,10 +168,11 @@ def createPointCloud(builder, header, height=960, width=1280):
     PointCloud2.AddWidth(builder, points.shape[1])
     PointCloud2.AddIsBigendian(builder, False)
     PointCloud2.AddPointStep(builder, 12)
-    PointCloud2.AddRowStep(builder, points.shape[0] * points.shape[1])
+    PointCloud2.AddRowStep(builder, points.shape[1], 12)
     PointCloud2.AddFields(builder, pointFieldVector)
     PointCloud2.AddData(builder, pointsVector)
     PointCloud2.AddLabelsGeneral(builder, labelsGeneralVector)
+    PointCloud2.AddLabelsBb(builder, labelsBbVector)
     return PointCloud2.End(builder)
 
 
@@ -121,11 +180,11 @@ def createPointClouds(projectUuid, numOf=10):
     '''Creates 'numOf' point cloud messages'''
     theTime = int(time.time())
     for i in range(numOf):
-        print(f"Send point cloud: {str(i)}")
+        print(f"Send point cloud: {str(i+1)}")
         builder = flatbuffers.Builder(1024)
 
         header = createHeader(builder, theTime + i, "camera", projectUuid)
-        pointCloudMsg = createPointCloud(builder, header)
+        pointCloudMsg = createPointCloud(builder, header, projectUuid)
         builder.Finish(pointCloudMsg)
         yield bytes(builder.Output())
 
