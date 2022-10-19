9ba03e86eb3653ba9b5a826878b34bb826dd2c9b
Julian Arkenau <julian.arkenau@dfki.de>
2022-10-19 07:55:57 +0000

    inital package structure for seerep-hdf5-ros

---
+++ seerep-hdf5/seerep-hdf5-ros/include/seerep-hdf5-ros/hdf5-ros.h
@@ -0,0 +1,16 @@
+#ifndef SEEREP_HDF5_ROS_H_
+#define SEEREP_HDF5_ROS_H_
+
+/* highfive */
+#include <highfive/H5DataSet.hpp>
+#include <highfive/H5File.hpp>
+
+namespace seerep_hdf5_ros
+{
+class Hdf5Ros
+{
+public:
+  Hdf5Ros() = default;
+};
+}  // namespace seerep_hdf5_ros
+#endif /* SEEREP_HDF5_ROS_H_ */
---
+++ seerep-hdf5/seerep-hdf5-ros/src/hdf5-ros.cpp
@@ -0,0 +1,1 @@
+#include "seerep-hdf5-ros/hdf5-ros.h"
