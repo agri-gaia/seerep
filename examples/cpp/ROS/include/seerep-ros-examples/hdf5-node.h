fc4e43c244779de4e33dca2384842e74bd5a89aa
Julian Arkenau <julian.arkenau@dfki.de>
2022-10-21 13:39:14 +0000

    inital structure for ROS Node to directly dump messages

---
+++ examples/cpp/ROS/include/seerep-ros-examples/hdf5-node.h
@@ -0,0 +1,18 @@
+#ifndef HDF5_DUMP_NODE_H
+#define HDF5_DUMP_NODE_H
+
+/* ros */
+#include "ros/master.h"
+#include "ros/ros.h"
+
+/* seerep-hdf5-ros */
+#include "seerep-hdf5-ros/hdf5-ros.h"
+
+namespace seerep_ros_examples
+{
+class DumpNode
+{
+};
+} /* namespace seerep_ros_examples */
+
+#endif /* HDF5_DUMP_NODE_H_ */
---
+++ examples/cpp/ROS/src/hdf5-node.cpp
@@ -0,0 +1,8 @@
+#include "seerep-ros-examples/hdf5-node.h"
+
+int main(int argc, char** argv)
+{
+  const std::string& node_name = "hdf5-node";
+  ros::init(argc, argv, node_name);
+  return 0;
+}
