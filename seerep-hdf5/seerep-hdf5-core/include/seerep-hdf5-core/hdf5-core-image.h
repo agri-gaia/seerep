6c2aa357d547b312377cc7a93b311f6f0a855824
Julian Arkenau <julian.arkenau@dfki.de>
2022-12-16 16:28:02 +0000

    docs: add code docs to hdf5-core-image

--- seerep-hdf5/seerep-hdf5-core/include/seerep-hdf5-core/hdf5-core-image.h
+++ seerep-hdf5/seerep-hdf5-core/include/seerep-hdf5-core/hdf5-core-image.h
@@ -24,6 +24,10 @@
 
 namespace seerep_hdf5_core
 {
+/**
+ * @brief Helper struct to summarize the general attributes of an image
+ *
+ */
 struct ImageAttributes
 {
   uint32_t height;
@@ -33,30 +37,91 @@ struct ImageAttributes
   bool isBigendian;
 };
 
+/**
+ * @brief This class encompasses all hdf5-io functions which are message type independent
+ *
+ * This means that the functions can currently be used for flatbuffers and protocol buffers
+ *
+ */
 class Hdf5CoreImage : public virtual Hdf5CoreGeneral, public Hdf5CoreDatatypeInterface
 {
 public:
+  /**
+   * @brief Construct a new Hdf 5 Core Image object
+   *
+   * @param file
+   * @param write_mtx
+   */
   Hdf5CoreImage(std::shared_ptr<HighFive::File>& file, std::shared_ptr<std::mutex>& write_mtx);
 
+  /**
+   * @brief Get the indices for the seerep core from an image data group
+   *
+   * @param uuid the uuid of the image data group
+   * @return std::optional<seerep_core_msgs::DatasetIndexable> the seerep core message with the indices
+   */
   std::optional<seerep_core_msgs::DatasetIndexable> readDataset(const boost::uuids::uuid& uuid);
+
+  /**
+   * @brief Get the indices for the seerep core from an image data group
+   *
+   * Used when recreating the indices on a server restart
+   *
+   * @param uuid the uuid of the image data group
+   * @return std::optional<seerep_core_msgs::DatasetIndexable> the seerep core message with the indices
+   */
   std::optional<seerep_core_msgs::DatasetIndexable> readDataset(const std::string& uuid);
 
+  /**
+   * @brief Get all of the data set UUIDs in an image data group
+   *
+   * @return std::vector<std::string> vector of UUIDs (strings)
+   */
   std::vector<std::string> getDatasetUuids();
 
+  /**
+   * @brief Write generals labels based on C++ data structures to HdF5
+   *
+   * @param uuid uuid of the image data group
+   * @param labels vector of labels
+   * @param instances vector of instances (string UUIDs)
+   */
   void writeLabelsGeneral(const std::string& uuid, const std::vector<std::string>& labels,
                           const std::vector<std::string>& instances);
 
+  /**
+   * @brief Write the general attributes of an image to hdf5
+   *
+   * @param id uuid of the image data group
+   * @param attributes struct with the general attributes
+   */
   void writeImageAttributes(const std::string& id, const ImageAttributes& attributes);
 
+  /**
+   * @brief Read general attributes of an image from hdf5
+   *
+   * @param id uuid of the image data group
+   * @return ImageAttributes struct with the general attributes
+   */
   ImageAttributes readImageAttributes(const std::string& id);
 
+  /**
+   * @brief Get the path to the hdf5 group of an image
+   *
+   * @param id uuid of the image data group
+   * @return const std::string the path to the image group
+   */
   const std::string getHdf5GroupPath(const std::string& id) const;
+
+  /**
+   * @brief Get the path to the image raw data dataset based on the data group uuid
+   *
+   * @param id uuid of the image data group
+   * @return const std::string path to the image dataset
+   */
   const std::string getHdf5DataSetPath(const std::string& id) const;
 
 public:
-  inline static const std::string SIZE = "size";
-  inline static const std::string CLASS = "CLASS";
-
   // image attribute keys
   inline static const std::string HEIGHT = "height";
   inline static const std::string WIDTH = "width";
@@ -67,7 +132,7 @@ public:
 
   inline static const std::string RAWDATA = "rawdata";
 
-  // datatype group names in hdf5
+  // datatype group name in hdf5
   inline static const std::string HDF5_GROUP_IMAGE = "images";
 };
 
