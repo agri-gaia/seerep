ba8a77ddd4a5cb0c64b0b824a78a609672c7fe09
Julian Arkenau <julian.arkenau@dfki.de>
2023-01-23 07:18:08 +0000

    ref: remove unused time functions from core-general

--- seerep-hdf5/seerep-hdf5-core/include/seerep-hdf5-core/hdf5-core-general.h
+++ seerep-hdf5/seerep-hdf5-core/include/seerep-hdf5-core/hdf5-core-general.h
@@ -71,25 +71,6 @@ public:
 
   bool hasAABB(const std::string& datatypeGroup, const std::string& uuid);
 
-  // ################
-  //  Time
-  // ################
-  void readTimeFromRaw(const std::string& datatypeGroup, const std::string& uuid, int64_t& secs, int64_t& nanos);
-  void readTime(const std::string& datatypeGroup, const std::string& uuid, int64_t& secs, int64_t& nanos);
-  template <class T>
-  void readTimeFromAnnotateTraits(const std::string& id, int64_t& value,
-                                  const HighFive::AnnotateTraits<T>& highFiveObject, const std::string& attribute);
-
-  void writeTimeToRaw(const std::string& datatypeGroup, const std::string& uuid, const int64_t& secs,
-                      const int64_t& nanos);
-  void writeTime(const std::string& datatypeGroup, const std::string& uuid, const int64_t& secs, const int64_t& nanos);
-  template <class T>
-  void writeTimeToAnnotateTraits(const int64_t& value, HighFive::AnnotateTraits<T>& highFiveObject,
-                                 const std::string& attribute);
-
-  bool hasTimeRaw(const std::string& datatypeGroup, const std::string& uuid);
-  bool hasTime(const std::string& datatypeGroup, const std::string& uuid);
-
   // BoundingBoxes
   /**
    * @brief read the labels and instances of a dataset of all categories and add them to the category-labels/instances map
