58170607905f449ab26f7e93b5c25d373d27e9b5
Hunaid Hameed <hunaid.hameed@dfki.de>
2023-01-24 09:13:03 +0000

    protobuf implementation

--- seerep-srv/seerep-server/src/pb-meta-operations.cpp
+++ seerep-srv/seerep-server/src/pb-meta-operations.cpp
@@ -77,4 +77,89 @@ grpc::Status PbMetaOperations::GetProjects(grpc::ServerContext* context, const g
   return grpc::Status::OK;
 }
 
+grpc::Status GetOverallTimeInterval(grpc::ServerContext* context, const seerep::UuidDatatypePair* request,
+                                    seerep::TimeInterval* response)
+{
+  (void)context;  // ignore that variable without causing warnings
+  BOOST_LOG_SEV(m_logger, boost::log::trivial::severity_level::debug) << "fetching overall time interval";
+
+  std::string uuid = request->projectuuid();
+  boost::uuids::string_generator gen;
+  auto uuidFromString = gen(uuid);
+
+  std::vector<seerep_core_msgs::Datatype> dt_vector;
+
+  if (request->datatype() == seerep::datatype::image)
+  {
+    dt_vector.push_back(seerep_core_msgs::Datatype::Image);
+  }
+  else if (request->datatype() == seerep::datatype::pointcloud)
+  {
+    dt_vector.push_back(seerep_core_msgs::Datatype::PointCloud);
+  }
+  else if (request->datatype() == seerep::datatype::point)
+  {
+    dt_vector.push_back(seerep_core_msgs::Datatype::Point);
+  }
+  else
+  {
+    dt_vector.push_back(seerep_core_msgs::Datatype::Unknown);
+  }
+
+  seerep_core_msgs::AabbTime timeinterval = seerepCore->getOverallTimeInterval(uuidFromString, dt_vector);
+
+  // isolate second and nano second bits from min time
+  uint64_t mintime = timeinterval.min_corner().get<0>();
+  uint32_t min_nanos = (uint32_t)mintime;
+  uint32_t min_seconds = (uint32_t)(mintime >> 32);
+
+  // isolate second and nano second bits from max time
+  uint64_t maxtime = timeinterval.max_corner().get<0>();
+  uint32_t max_nanos = (uint32_t)maxtime;
+  uint32_t max_seconds = (uint32_t)(maxtime >> 32);
+
+  response->set_time_min(mintime);
+  response->set_time_max(maxtime);
+
+  return grpc::Status::OK;
+}
+grpc::Status GetOverallBoundingBox(grpc::ServerContext* context, const seerep::UuidDatatypePair* request,
+                                   seerep::Boundingbox* response)
+{
+  (void)context;  // ignore that variable without causing warnings
+  BOOST_LOG_SEV(m_logger, boost::log::trivial::severity_level::debug) << "fetching overall bounding box";
+
+  std::string uuid = request->projectuuid();
+  boost::uuids::string_generator gen;
+  auto uuidFromString = gen(uuid);
+
+  std::vector<seerep_core_msgs::Datatype> dt_vector;
+
+  if (request->datatype() == seerep::datatype::image)
+  {
+    dt_vector.push_back(seerep_core_msgs::Datatype::Image);
+  }
+  else if (request->datatype() == seerep::datatype::pointcloud)
+  {
+    dt_vector.push_back(seerep_core_msgs::Datatype::PointCloud);
+  }
+  else if (request->datatype() == seerep::datatype::point)
+  {
+    dt_vector.push_back(seerep_core_msgs::Datatype::Point);
+  }
+  else
+  {
+    dt_vector.push_back(seerep_core_msgs::Datatype::Unknown);
+  }
+
+  seerep_core_msgs::AABB overallBB = seerepCore->getOverallBound(uuidFromString, dt_vector);
+
+  flatbuffers::grpc::MessageBuilder builder;
+
+  response->set_min_corner(overallBB.min_corner());
+  response->set_max_corner(overallBB.max_corner());
+
+  return grpc::Status::OK;
+}
+
 } /* namespace seerep_server */
