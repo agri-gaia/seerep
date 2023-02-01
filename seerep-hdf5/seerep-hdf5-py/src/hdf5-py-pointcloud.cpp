3e88810df194f501e698ff8ea57105691f6ace8a
Malte kl. Piening <mkl.piening@gmail.com>
2023-02-01 14:30:49 +0000

    fixed channel offset

--- seerep-hdf5/seerep-hdf5-py/src/hdf5-py-pointcloud.cpp
+++ seerep-hdf5/seerep-hdf5-py/src/hdf5-py-pointcloud.cpp
@@ -50,30 +50,6 @@ void Hdf5PyPointCloud::writePointCloud(const std::string& uuid, const std::strin
     }
   }
 
-  // write basic attributes
-
-  std::size_t point_step = channels.size() * sizeof(float);
-  if (!m_file->exist(cloud_group_id))
-  {
-    data_group_ptr = std::make_shared<HighFive::Group>(m_file->createGroup(cloud_group_id));
-    data_group_ptr->createAttribute(seerep_hdf5_core::Hdf5CorePointCloud::HEIGHT, 1);
-    data_group_ptr->createAttribute(seerep_hdf5_core::Hdf5CorePointCloud::WIDTH, num_points);
-    data_group_ptr->createAttribute(seerep_hdf5_core::Hdf5CorePointCloud::IS_BIGENDIAN, false);
-    data_group_ptr->createAttribute(seerep_hdf5_core::Hdf5CorePointCloud::POINT_STEP, point_step);
-    data_group_ptr->createAttribute(seerep_hdf5_core::Hdf5CorePointCloud::ROW_STEP, num_points * point_step);
-    data_group_ptr->createAttribute(seerep_hdf5_core::Hdf5CorePointCloud::IS_DENSE, true);
-  }
-  else
-  {
-    data_group_ptr = std::make_shared<HighFive::Group>(m_file->getGroup(cloud_group_id));
-    data_group_ptr->getAttribute(seerep_hdf5_core::Hdf5CorePointCloud::HEIGHT).write(1);
-    data_group_ptr->getAttribute(seerep_hdf5_core::Hdf5CorePointCloud::WIDTH).write(num_points);
-    data_group_ptr->getAttribute(seerep_hdf5_core::Hdf5CorePointCloud::IS_BIGENDIAN).write(false);
-    data_group_ptr->getAttribute(seerep_hdf5_core::Hdf5CorePointCloud::POINT_STEP).write(point_step);
-    data_group_ptr->getAttribute(seerep_hdf5_core::Hdf5CorePointCloud::ROW_STEP).write(num_points * point_step);
-    data_group_ptr->getAttribute(seerep_hdf5_core::Hdf5CorePointCloud::IS_DENSE).write(true);
-  }
-
   // write header
 
   if (!data_group_ptr->hasAttribute(seerep_hdf5_core::Hdf5CoreGeneral::HEADER_STAMP_SECONDS))
@@ -175,16 +151,14 @@ void Hdf5PyPointCloud::writePointCloud(const std::string& uuid, const std::strin
         break;
     }
 
+    uint32_t num_elements = 1;
     if (channel_info.shape.size() > 1)
     {
-      counts.push_back(channel_info.shape[1]);
+      num_elements = channel_info.shape[1];
     }
-    else
-    {
-      counts.push_back(1);
-    }
+    counts.push_back(num_elements);
 
-    offset += data.dtype().itemsize();
+    offset += data.dtype().itemsize() * num_elements;
   }
 
   seerep_hdf5_core::Hdf5CoreGeneral::writeAttributeToHdf5(*data_group_ptr,
@@ -196,6 +170,29 @@ void Hdf5PyPointCloud::writePointCloud(const std::string& uuid, const std::strin
   seerep_hdf5_core::Hdf5CoreGeneral::writeAttributeToHdf5(*data_group_ptr,
                                                           seerep_hdf5_core::Hdf5CorePointCloud::FIELD_COUNT, counts);
 
+  // write basic attributes
+
+  if (!m_file->exist(cloud_group_id))
+  {
+    data_group_ptr = std::make_shared<HighFive::Group>(m_file->createGroup(cloud_group_id));
+    data_group_ptr->createAttribute(seerep_hdf5_core::Hdf5CorePointCloud::HEIGHT, 1);
+    data_group_ptr->createAttribute(seerep_hdf5_core::Hdf5CorePointCloud::WIDTH, num_points);
+    data_group_ptr->createAttribute(seerep_hdf5_core::Hdf5CorePointCloud::IS_BIGENDIAN, false);
+    data_group_ptr->createAttribute(seerep_hdf5_core::Hdf5CorePointCloud::POINT_STEP, offset);
+    data_group_ptr->createAttribute(seerep_hdf5_core::Hdf5CorePointCloud::ROW_STEP, num_points * offset);
+    data_group_ptr->createAttribute(seerep_hdf5_core::Hdf5CorePointCloud::IS_DENSE, true);
+  }
+  else
+  {
+    data_group_ptr = std::make_shared<HighFive::Group>(m_file->getGroup(cloud_group_id));
+    data_group_ptr->getAttribute(seerep_hdf5_core::Hdf5CorePointCloud::HEIGHT).write(1);
+    data_group_ptr->getAttribute(seerep_hdf5_core::Hdf5CorePointCloud::WIDTH).write(num_points);
+    data_group_ptr->getAttribute(seerep_hdf5_core::Hdf5CorePointCloud::IS_BIGENDIAN).write(false);
+    data_group_ptr->getAttribute(seerep_hdf5_core::Hdf5CorePointCloud::POINT_STEP).write(offset);
+    data_group_ptr->getAttribute(seerep_hdf5_core::Hdf5CorePointCloud::ROW_STEP).write(num_points * offset);
+    data_group_ptr->getAttribute(seerep_hdf5_core::Hdf5CorePointCloud::IS_DENSE).write(true);
+  }
+
   m_file->flush();
 }
 
@@ -203,8 +200,7 @@ void Hdf5PyPointCloud::writePoints(const std::string& cloud_group_id, std::map<s
                                    const std::map<std::string, py::array>& channels)
 {
   std::vector<std::vector<std::string>> channel_names({ { "xyz" }, { "x", "y", "z" } });
-  if (!writeChannelTyped<uint8_t, uint16_t, float, double>(cloud_group_id, "points", channel_names, processed, channels,
-                                                           true))
+  if (!writeChannelTyped<float, double>(cloud_group_id, "points", channel_names, processed, channels, true))
   {
     // no channels found
     throw std::invalid_argument("you need to either specify 'x', 'y' and 'z' channels individually or a 'xyz' channel");
