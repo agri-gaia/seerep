5955fba1a844454005a588078b6af99accdcb643
Malte kl. Piening <mkl.piening@gmail.com>
2023-02-02 11:08:45 +0000

    added image meta writing

--- seerep-hdf5/seerep-hdf5-py/src/hdf5-py-image.cpp
+++ seerep-hdf5/seerep-hdf5-py/src/hdf5-py-image.cpp
@@ -15,7 +15,87 @@ Hdf5PyImage::Hdf5PyImage(Hdf5FileWrapper& hdf5_file)
 void Hdf5PyImage::writeImage(const std::string& uuid, const std::string& frame_id, int64_t seconds, int32_t nanos,
                              uint32_t sequence, const py::array& image)
 {
-  // TODO
+  const std::scoped_lock lock(*m_write_mtx);
+
+  py::buffer_info image_buff_info = image.request();
+
+  std::size_t num_channels = 1;
+  if (image_buff_info.shape.size() > 2)
+  {
+    num_channels = image_buff_info.shape[2];
+  }
+
+  std::string encoding = "";
+  if (num_channels == 1)
+  {
+    encoding = "mono";
+  }
+  else if (num_channels == 3)
+  {
+    encoding = "rgb";
+  }
+  else if (num_channels == 4)
+  {
+    encoding = "rgba";
+  }
+  else
+  {
+    throw std::invalid_argument("image must have either 1 (MONO), 3 (RGB) or 4 (RGBA) channels");
+  }
+
+  std::string hdf5GroupPath = getHdf5GroupPath(uuid);
+  std::string hdf5DataSetPath = getHdf5DataSetPath(uuid);
+
+  HighFive::DataSpace dataSpace({ image_buff_info.shape[0] * image_buff_info.shape[1] * num_channels });
+  auto dataGroupPtr = getHdf5Group(hdf5GroupPath);
+
+  std::shared_ptr<HighFive::DataSet> dataSetPtr = nullptr;
+
+  std::size_t channel_size = 0;  // element (channel data) size in bytes
+  switch (image.dtype().char_())
+  {
+    case 'B':
+      // image data is uint8_t
+      dataSetPtr = getHdf5DataSet<uint8_t>(hdf5DataSetPath, dataSpace);
+      channel_size = sizeof(uint8_t);
+
+      // TODO: write data
+      break;
+
+    case 'H':
+      // image data is uint16_t
+      dataSetPtr = getHdf5DataSet<uint16_t>(hdf5DataSetPath, dataSpace);
+      encoding = "rgb16";
+      channel_size = sizeof(uint16_t);
+
+      // TODO: write data
+      break;
+
+    default:
+      throw std::invalid_argument("image data dtype needs to be either 8 or 16 bit unsigned integer");
+  }
+
+  // append amount of bits per channel to encoding name
+  encoding = encoding + std::to_string(8 * channel_size);
+
+  // writeHeaderAttributes(*dataGroupPtr, image.header());
+
+  seerep_hdf5_core::ImageAttributes imageAttributes = { image_buff_info.shape[0], image_buff_info.shape[1],
+                                                        image_buff_info.shape[1] * num_channels * channel_size,
+                                                        encoding, false };
+
+  writeImageAttributes(uuid, imageAttributes);
+
+  // const uint8_t* arrayStartPtr = reinterpret_cast<const uint8_t*>(image.data().c_str());
+  // dataSetPtr->write(std::vector<uint8_t>(arrayStartPtr, arrayStartPtr + image.data().size()));
+
+  // TODO: write labels
+
+  // writeBoundingBox2DLabeled(seerep_hdf5_core::Hdf5CoreImage::HDF5_GROUP_IMAGE, id, image.labels_bb());
+  // seerep_hdf5_pb::Hdf5PbGeneral::writeLabelsGeneral(seerep_hdf5_core::Hdf5CoreImage::HDF5_GROUP_IMAGE, id,
+  //                                                   image.labels_general());
+
+  m_file->flush();
 }
 
 py::array Hdf5PyImage::readImage(const std::string& uuid)
