cfb0158be2df2ef3621431e6b5d07a12fcac2dd5
Marc Meijer <marc.meijer@dfki.de>
2023-06-26 22:59:01 +0200

    rebase against main

--- examples/python/gRPC/images/gRPC_fb_addCameraIntrinsics.py
+++ examples/python/gRPC/images/gRPC_fb_addCameraIntrinsics.py
@@ -15,48 +15,51 @@ from seerep.util.fb_helper import (
     getProject,
 )
 
-builder = flatbuffers.Builder(1000)
-# Default server is localhost !
-channel = get_gRPC_channel()
 
-# 1. Get all projects from the server
-projectuuid = getProject(builder, channel, 'testproject')
+def get_ciuuid():
+    builder = flatbuffers.Builder(1000)
+    # Default server is localhost !
+    channel = get_gRPC_channel()
 
-ciuuid = str(uuid.uuid4())
-print("Camera Intrinsics will be saved against the uuid: ", ciuuid)
+    # 1. Get all projects from the server
+    projectuuid = getProject(builder, channel, "testproject")
 
-# 2. Check if the defined project exist; if not exit
-if not projectuuid:
-    print("project doesn't exist!")
-    exit()
+    ciuuid = str(uuid.uuid4())
+    print("Camera Intrinsics will be saved against the uuid: ", ciuuid)
 
-# 3. Get gRPC service object
-stub = ci_service.CameraIntrinsicsServiceStub(channel)
+    # 2. Check if the defined project exist; if not exit
+    if not projectuuid:
+        print("project doesn't exist!")
+        exit()
 
-# Create all necessary objects for the query
-ts = createTimeStamp(builder, 4, 3)
-header = createHeader(builder, ts, "map", projectuuid, ciuuid)
-roi = createRegionOfInterest(builder, 3, 5, 6, 7, True)
+    # 3. Get gRPC service object
+    stub = ci_service.CameraIntrinsicsServiceStub(channel)
 
-matrix = [4, 5, 6]
-ci = createCameraIntrinsics(builder, header, 3, 4, "plump_bob", matrix, matrix, matrix, matrix, 4, 5, roi, 5)
-builder.Finish(ci)
+    # Create all necessary objects for the query
+    ts = createTimeStamp(builder, 4, 3)
+    header = createHeader(builder, ts, "map", projectuuid, ciuuid)
+    roi = createRegionOfInterest(builder, 3, 5, 6, 7, True)
 
-buf = builder.Output()
+    matrix = [4, 5, 6]
+    ci = createCameraIntrinsics(builder, header, 3, 4, "plump_bob", matrix, matrix, matrix, matrix, 4, 5, roi, 5)
+    builder.Finish(ci)
 
-stub.TransferCameraIntrinsics(bytes(buf))
+    buf = builder.Output()
 
-# Fetch the saved CI
-builder = flatbuffers.Builder(1000)
+    stub.TransferCameraIntrinsics(bytes(buf))
 
-ci_query = createCameraIntrinsicsQuery(builder, ciuuid, projectuuid)
+    # Fetch the saved CI
+    builder = flatbuffers.Builder(1000)
 
-builder.Finish(ci_query)
-buf = builder.Output()
+    ci_query = createCameraIntrinsicsQuery(builder, ciuuid, projectuuid)
 
-ret = stub.GetCameraIntrinsics(bytes(buf))
+    builder.Finish(ci_query)
+    buf = builder.Output()
 
-retrieved_ci = CameraIntrinsics.CameraIntrinsics.GetRootAs(ret)
+    ret = stub.GetCameraIntrinsics(bytes(buf))
 
-# printing the uuid of the retrieved camera intrinsics
-print(retrieved_ci.Header().UuidMsgs().decode('utf-8'))
+    retrieved_ci = CameraIntrinsics.CameraIntrinsics.GetRootAs(ret)
+
+    # printing the uuid of the retrieved camera intrinsics
+    print(retrieved_ci.Header().UuidMsgs().decode("utf-8"))
+    return ciuuid
---
+++ examples/tests/python/gRPC/images/test_gRPC_pb_queryImage.py
@@ -0,0 +1,1 @@
+# todo: documentation for installation of pytest
