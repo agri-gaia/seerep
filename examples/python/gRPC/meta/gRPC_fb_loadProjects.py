ad319ca8673a8019d881c0f061b3a1f2193e1ef7
Julian Arkenau <julian.arkenau@dfki.de>
2023-07-12 15:16:03 +0200

    print returned projects in fb loadproject example

--- examples/python/gRPC/meta/gRPC_fb_loadProjects.py
+++ examples/python/gRPC/meta/gRPC_fb_loadProjects.py
@@ -3,6 +3,7 @@
 import flatbuffers
 from seerep.fb.Empty import Empty
 from seerep.fb.meta_operations_grpc_fb import MetaOperationsStub
+from seerep.fb.ProjectInfos import ProjectInfos
 from seerep.util.common import get_gRPC_channel
 from seerep.util.fb_helper import createEmpty
 
@@ -12,7 +13,8 @@ stub = MetaOperationsStub(channel)
 
 fbb = flatbuffers.Builder(1024)
 responseBuf = stub.LoadProjects(bytes(createEmpty(fbb)))
-responseBuf = Empty.GetRootAs(responseBuf)
+responseBuf = ProjectInfos.GetRootAs(responseBuf)
 
 if responseBuf:
-    print("Projects loaded successfully.")
+    for i in range(responseBuf.ProjectsLength()):
+        print(responseBuf.Projects(i).Name().decode("utf-8") + " " + responseBuf.Projects(i).Uuid().decode("utf-8"))
