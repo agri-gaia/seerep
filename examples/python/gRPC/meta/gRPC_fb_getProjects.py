232d4ea240561a98a9ea4766b65a2bff547ef02a
Marc Meijer <marc.meijer@dfki.de>
2023-09-11 11:56:54 +0200

    added tests for geodeticCoordinate projects

--- examples/python/gRPC/meta/gRPC_fb_getProjects.py
+++ examples/python/gRPC/meta/gRPC_fb_getProjects.py
@@ -1,23 +1,39 @@
 #!/usr/bin/env python3
+from typing import List
 
 import flatbuffers
+from grpc import Channel
 from seerep.fb import Empty, ProjectInfos
 from seerep.fb import meta_operations_grpc_fb as metaOperations
 from seerep.util.common import get_gRPC_channel
 
-channel = get_gRPC_channel()
 
-stub = metaOperations.MetaOperationsStub(channel)
+def get_projects(grpc_channel: Channel = get_gRPC_channel()) -> List:
+    stub = metaOperations.MetaOperationsStub(grpc_channel)
 
-builder = flatbuffers.Builder(1024)
-Empty.Start(builder)
-emptyMsg = Empty.End(builder)
-builder.Finish(emptyMsg)
-buf = builder.Output()
+    builder = flatbuffers.Builder(1024)
+    Empty.Start(builder)
+    emptyMsg = Empty.End(builder)
+    builder.Finish(emptyMsg)
+    buf = builder.Output()
 
-responseBuf = stub.GetProjects(bytes(buf))
-response = ProjectInfos.ProjectInfos.GetRootAs(responseBuf)
+    responseBuf = stub.GetProjects(bytes(buf))
+    response = ProjectInfos.ProjectInfos.GetRootAs(responseBuf)
 
-print("The server has the following projects (name/uuid):")
-for i in range(response.ProjectsLength()):
-    print("\t" + response.Projects(i).Name().decode("utf-8") + " " + response.Projects(i).Uuid().decode("utf-8"))
+    projects_list: List = []
+
+    print("The server has the following projects (name/uuid):")
+    for i in range(response.ProjectsLength()):
+        print(
+            "\t"
+            + response.Projects(i).Name().decode("utf-8")
+            + " "
+            + response.Projects(i).Uuid().decode("utf-8")
+        )
+        projects_list.append(response.Projects(i))
+
+    return projects_list
+
+
+if __name__ == "__main__":
+    get_projects()
---
+++ examples/tests/python/gRPC/meta/test_gRPC_fb_createGeodeticCoordProject.py
@@ -0,0 +1,59 @@
+from typing import List
+
+import fb_to_dict
+import flatbuffers
+import seerep.util.fb_helper as fb_helper
+from gRPC.meta import gRPC_fb_createGeodeticCoordProject as project_creation
+from gRPC.meta import gRPC_fb_getProjects as projects_query
+from seerep.fb import ProjectInfo as PI
+
+
+def test_gRPC_fb_createGeoProjectAndGetProjects(grpc_channel):
+    # get already created projects
+    current_projects = projects_query.get_projects(grpc_channel)
+
+    # contains the flatbuffers buffer objects of the projects
+    created_projects: List[PI.ProjectInfo] = []
+    # create_project returns Tuple[str, str] = (<project_name>, <project_uuid>)
+    created_projects.append(project_creation.create_geo_proj(grpc_channel))
+    created_projects.append(project_creation.create_geo_proj(grpc_channel))
+
+    # get a list of all created projects
+    projects_list: List[PI.ProjectInfo] = projects_query.get_projects(grpc_channel)
+
+    created_projects_dicts = [
+        fb_to_dict.fb_obj_to_dict(p_dict) for p_dict in created_projects
+    ]
+    queried_projects_dicts = [
+        fb_to_dict.fb_obj_to_dict(p_dict) for p_dict in projects_list
+    ]
+
+    # check if count of created projects matches the count of the newly queried projects,
+    # returned by the gRPC_pb_getProjects example
+    assert len(projects_list) - len(current_projects) == len(created_projects)
+
+    # manually check if the project parameters are correct
+    for proj_info in created_projects:
+        proj_name = proj_info.Name().decode("utf-8")
+        proj_uuid = proj_info.Uuid().decode("utf-8")
+        assert proj_name == "geodeticProject"
+        assert proj_uuid != ""
+        assert proj_info.Frameid().decode("utf-8") == "2"
+        assert (
+            proj_info.GeodeticPosition().CoordinateSystem().decode("utf-8")
+            == "EPSG::4326"
+        )
+        # missing
+        # assert proj_info.Datum().decode("utf-8") == "EPSG::7030"
+        assert proj_info.GeodeticPosition().Longitude() == 4
+        assert proj_info.GeodeticPosition().Latitude() == 4
+        assert proj_info.GeodeticPosition().Altitude() == 4
+        assert proj_info.Version() is not None
+
+    print(created_projects_dicts)
+    for proj_info in created_projects_dicts:
+        assert proj_info in queried_projects_dicts
+
+        # teardown
+        builder = flatbuffers.Builder(1024)
+        fb_helper.deleteProject(grpc_channel, builder, proj_name, proj_uuid)
