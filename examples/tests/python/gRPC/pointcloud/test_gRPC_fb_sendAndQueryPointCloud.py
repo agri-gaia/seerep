7f96b7860a00982f38542aee40172ed47252c036
Marc Meijer <marc.meijer@dfki.de>
2023-10-25 14:22:14 +0200

    add test script for pc2

---
+++ examples/tests/python/gRPC/pointcloud/test_gRPC_fb_sendAndQueryPointCloud.py
@@ -0,0 +1,28 @@
+from typing import List
+
+import fb_to_dict
+from gRPC.pointcloud import gRPC_fb_queryPointCloud as query_pc
+from gRPC.pointcloud import gRPC_fb_sendPointCloud as send_pc
+
+
+# todo: test query dependant on tf
+def test_sendAndQueryPCs(grpc_channel, project_setup):
+    proj_name, proj_uuid = project_setup
+
+    # send pointclouds to the project
+    sent_pcs = [
+        fb_to_dict.fb_obj_to_dict(elem)
+        for elem in sorted(
+            send_pc.send_pointcloud(proj_uuid, grpc_channel),
+            key=lambda pc: pc.Header().UuidMsgs().decode(),
+        )
+    ]
+    queried_pcs = [
+        fb_to_dict.fb_obj_to_dict(elem)
+        for elem in sorted(
+            query_pc.query_pcs(proj_uuid, grpc_channel),
+            key=lambda pc: pc.Header().UuidMsgs().decode(),
+        )
+    ]
+
+    assert queried_pcs == sent_pcs
