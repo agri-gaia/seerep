bc0b978064800f28ef4da8dc46f195bed50e5de4
Marc Meijer <marc.meijer@dfki.de>
2023-10-29 23:39:47 +0100

    added tests for laterAddedBB

--- examples/python/gRPC/pointcloud/gRPC_fb_addBoundingBox.py
+++ examples/python/gRPC/pointcloud/gRPC_fb_addBoundingBox.py
@@ -2,10 +2,12 @@
 NUM_BB_LABELS = 5
 
 import uuid
+from typing import List
 
 import flatbuffers
 import numpy as np
-from seerep.fb import PointCloud2
+from grpc import Channel
+from seerep.fb import BoundingBoxesLabeledStamped, PointCloud2
 from seerep.fb import point_cloud_service_grpc_fb as pointcloudService
 from seerep.util.common import get_gRPC_channel
 from seerep.util.fb_helper import (
@@ -20,61 +22,104 @@ from seerep.util.fb_helper import (
     getProject,
 )
 
-builder = flatbuffers.Builder(1024)
-channel = get_gRPC_channel()
 
-projectuuid = getProject(builder, channel, 'testproject')
+def add_pc_bounding_boxes(
+    target_proj_uuid: str = None, grpc_channel: Channel = get_gRPC_channel()
+):
 
-if not projectuuid:
-    print("Requested project does not exist")
-    exit()
+    builder = flatbuffers.Builder(1024)
 
-stub = pointcloudService.PointCloudServiceStub(channel)
+    if target_proj_uuid == None:
+        target_proj_uuid = getProject(builder, grpc_channel, "testproject")
 
-query = createQuery(builder, projectUuids=[builder.CreateString(projectuuid)], withoutData=True)
-builder.Finish(query)
-buf = builder.Output()
+        if target_proj_uuid == None:
+            print("Requested project does not exist")
+            exit()
 
-msgToSend = []
+    stub = pointcloudService.PointCloudServiceStub(grpc_channel)
 
-for responseBuf in stub.GetPointCloud2(bytes(buf)):
-    response = PointCloud2.PointCloud2.GetRootAs(responseBuf)
-
-    header = createHeader(
-        builder,
-        projectUuid=response.Header().UuidProject().decode("utf-8"),
-        msgUuid=response.Header().UuidMsgs().decode("utf-8"),
+    query = createQuery(
+        builder, projectUuids=[builder.CreateString(target_proj_uuid)], withoutData=True
     )
-
-    # create bounding box labels
-    x1, y1, z1 = np.random.rand(3)
-    x2, y2, z2 = np.random.rand(3)
-
-    print("SENDING x1, y1, z1: ", x1, " , ", y1, " , ", z1)
-    print("SENDING x2, y2, z2: ", x2, " , ", y2, " , ", z2)
-
-    boundingBoxes = createBoundingBoxes(
-        builder,
-        [createPoint(builder, x1, y1, z1) for _ in range(NUM_BB_LABELS)],
-        [createPoint(builder, x2, y2, z2) for _ in range(NUM_BB_LABELS)],
-    )
-    labelWithInstances = createLabelsWithInstance(
-        builder,
-        ["BoundingBoxLabel" + str(i) for i in range(NUM_BB_LABELS)],
-        [1.0 / (i + 0.1) for i in range(NUM_BB_LABELS)],
-        [str(uuid.uuid4()) for _ in range(NUM_BB_LABELS)],
-    )
-    labelsBb = createBoundingBoxesLabeled(builder, labelWithInstances, boundingBoxes)
-
-    boundingBoxLabeledWithCategory = createBoundingBoxLabeledWithCategory(
-        builder, builder.CreateString("laterAddedBB"), labelsBb
-    )
-
-    labelsBbVector = createBoundingBoxLabeledStamped(builder, header, [boundingBoxLabeledWithCategory])
-    builder.Finish(labelsBbVector)
+    builder.Finish(query)
     buf = builder.Output()
 
-    msgToSend.append(bytes(buf))
+    msgToSend = []
+    bbs_list: List[BoundingBoxesLabeledStamped.BoundingBoxesLabeledStamped] = []
+
+    for responseBuf in stub.GetPointCloud2(bytes(buf)):
+        response = PointCloud2.PointCloud2.GetRootAs(responseBuf)
+
+        header = createHeader(
+            builder,
+            projectUuid=response.Header().UuidProject().decode("utf-8"),
+            msgUuid=response.Header().UuidMsgs().decode("utf-8"),
+        )
+
+        # create bounding box labels
+        x1, y1, z1 = np.random.rand(3)
+        x2, y2, z2 = np.random.rand(3)
+
+        boundingBoxes = createBoundingBoxes(
+            builder,
+            [createPoint(builder, x1, y1, z1) for _ in range(NUM_BB_LABELS)],
+            [createPoint(builder, x2, y2, z2) for _ in range(NUM_BB_LABELS)],
+        )
+        labelWithInstances = createLabelsWithInstance(
+            builder,
+            ["BoundingBoxLabel" + str(i) for i in range(NUM_BB_LABELS)],
+            [1.0 / (i + 0.1) for i in range(NUM_BB_LABELS)],
+            [str(uuid.uuid4()) for _ in range(NUM_BB_LABELS)],
+        )
+        labelsBb = createBoundingBoxesLabeled(
+            builder, labelWithInstances, boundingBoxes
+        )
+
+        boundingBoxLabeledWithCategory = createBoundingBoxLabeledWithCategory(
+            builder, builder.CreateString("laterAddedBB"), labelsBb
+        )
+
+        labelsBbVector = createBoundingBoxLabeledStamped(
+            builder, header, [boundingBoxLabeledWithCategory]
+        )
+        builder.Finish(labelsBbVector)
+        buf = builder.Output()
+
+        bbs_list.append(
+            BoundingBoxesLabeledStamped.BoundingBoxesLabeledStamped.GetRootAs(buf)
+        )
+        msgToSend.append(bytes(buf))
+
+    response = stub.AddBoundingBoxesLabeled(iter(msgToSend))
+    return bbs_list
 
 
-stub.AddBoundingBoxesLabeled(iter(msgToSend))
+if __name__ == "__main__":
+    bbs_lst: List[
+        BoundingBoxesLabeledStamped.BoundingBoxesLabeledStamped
+    ] = add_pc_bounding_boxes()
+
+    for bbs in bbs_lst:
+        for label_idx in range(bbs.LabelsBbLength()):
+            for bb_idx in range(bbs.LabelsBb(label_idx).BoundingBoxLabeledLength()):
+                uuid = bbs.Header().UuidMsgs().decode()
+                print(f"uuid: {uuid}")
+                cp = (
+                    bbs.LabelsBb(label_idx)
+                    .BoundingBoxLabeled(bb_idx)
+                    .BoundingBox()
+                    .CenterPoint()
+                )
+                se = (
+                    bbs.LabelsBb(label_idx)
+                    .BoundingBoxLabeled(bb_idx)
+                    .BoundingBox()
+                    .SpatialExtent()
+                )
+
+                print(
+                    f"    SENT CENTER POINT (x1, y1, z1): ({cp.X()}, {cp.Y()}, {cp.Z()})"
+                )
+                print(
+                    f"    SENT SPATIAL EXTENT (x1, y1, z1): ({se.X()}, {se.Y()}, {se.Z()})"
+                )
---
+++ examples/tests/python/gRPC/pointcloud/test_gRPC_fb_addBoundingBox.py
@@ -0,0 +1,56 @@
+import fb_to_dict
+from gRPC.pointcloud import gRPC_fb_addBoundingBox as add_bb
+from gRPC.pointcloud import gRPC_fb_queryPointCloud as query_pc
+from gRPC.pointcloud import gRPC_fb_sendPointCloud as send_pc
+
+
+def test_addBoundingBoxToPCs(grpc_channel, project_setup):
+    proj_name, proj_uuid = project_setup
+
+    # send pointclouds to the project
+    send_pc.send_pointcloud(proj_uuid, grpc_channel)
+
+    # add bbs to the point clouds
+    bbs_ls = [
+        fb_to_dict.fb_obj_to_dict(elem)
+        for elem in sorted(
+            add_bb.add_pc_bounding_boxes(proj_uuid, grpc_channel),
+            key=lambda bb: bb.Header().UuidMsgs().decode(),
+        )
+    ]
+
+    # query pointclouds
+    queried_pcs = [
+        fb_to_dict.fb_obj_to_dict(elem)
+        for elem in sorted(
+            query_pc.query_pcs(proj_uuid, grpc_channel),
+            key=lambda pc: pc.Header().UuidMsgs().decode(),
+        )
+    ]
+
+    default_rot = {"W": 1.0, "X": 0.0, "Y": 0.0, "Z": 0.0}
+
+    # sort inner bb list and add rotation to the values as this is set with default values by the server
+    for bbs in bbs_ls:
+        for label in bbs["LabelsBb"]:
+            label["BoundingBoxLabeled"].sort(
+                key=lambda bb: bb["LabelWithInstance"]["InstanceUuid"]
+            )
+            for bb in label["BoundingBoxLabeled"]:
+                bb["BoundingBox"]["Rotation"] = default_rot
+
+    for bbs in queried_pcs:
+        for label in bbs["LabelsBb"]:
+            label["BoundingBoxLabeled"].sort(
+                key=lambda bb: bb["LabelWithInstance"]["InstanceUuid"]
+            )
+
+    assert len(queried_pcs) == len(bbs_ls)
+
+    for idx, pc in enumerate(queried_pcs):
+        # filter for labels in category laterAddedBB
+        pc_labeled_bb_with_category = [
+            elem for elem in pc["LabelsBb"] if elem["Category"] == "laterAddedBB"
+        ]
+        assert pc["Header"]["UuidMsgs"] == bbs_ls[idx]["Header"]["UuidMsgs"]
+        assert pc_labeled_bb_with_category == bbs_ls[idx]["LabelsBb"]
