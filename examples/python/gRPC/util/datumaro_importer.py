661f0b053c0b880ab5379c650d52aca0bbf9d76b
Julian Arkenau <julian.arkenau@dfki.de>
2024-02-20 16:20:06 +0100

    WIP to importer

--- examples/python/gRPC/util/datumaro_importer.py
+++ examples/python/gRPC/util/datumaro_importer.py
@@ -19,10 +19,10 @@ from seerep.util.fb_helper import createTimeStamp as create_fb_timestamp
 
 DATUMARO_JSON_PATH = "/seerep/seerep-data/datumaro_test/annotations/default.json"
 IMAGE_FOLDER_PATH = "/seerep/seerep-data/datumaro_test/images/default/"
-FRAME__ID = "map"
+FRAME_ID = "map"
 # NOTE: Mock data
-CAMERA_INTRINSIC_UUID = "d0337928-6fba-4b3f-a8e1-65f52c0da5de"
-PROJECT_UUID = "67b3fa5c-298b-4cff-8af6-f0444ebc2f89"
+CAMERA_INTRINSIC_UUID = "7c8c6037-93a4-4b78-a5d3-c62d4b21fa57"
+PROJECT_UUID = "ce9dbda6-63f3-4c6d-81b3-41f39f031647"
 
 
 class DatumaroImporter:
@@ -50,43 +50,39 @@ class DatumaroImporter:
         return create_fb_label_category(flatbuffers.Builder(1024), labels, annotations, category)
 
     # NOTE: currently only works with pngs
-    def create_image(self, image_info: dict, category_labels: List[LabelCategory.LabelCategory]) -> Image.Image:
+    def create_image(self, fbb, image_info: dict, category_labels: List[LabelCategory.LabelCategory]) -> Image.Image:
         image_path = self.image_folder.joinpath(Path(image_info['path']))
         if not self.valid_file(image_path, "png"):
             raise ValueError('Invalid image path')
         cv_image = cv2.imread(str(image_path))
 
-        fb_timestamp = create_fb_timestamp(flatbuffers.Builder(1024), int(time.time()))
-        fb_header = create_fb_header(flatbuffers.Builder(1024), fb_timestamp, FRAME__ID, PROJECT_UUID)
-        fbb = flatbuffers.Builder(1024)
+        fb_timestamp = create_fb_timestamp(fbb, int(time.time()))
+        fb_header = create_fb_header(fbb, fb_timestamp, FRAME_ID, PROJECT_UUID)
+        # TODO: add labels
         fb_image = create_fb_image(fbb, fb_header, "png", False, cv_image, CAMERA_INTRINSIC_UUID, category_labels)
         fbb.Finish(fb_image)
         return fbb.Output()
 
     def iterate_json(self, data: dict) -> Generator[bytes, None, None]:
+        fbb = flatbuffers.Builder(1024)
         for item in data['items']:
             label_ids = set([annotation['label_id'] for annotation in item['annotations']])
-            fb_labels = [
-                create_fb_label(flatbuffers.Builder(1024), list(self.labels.keys())[label_id], label_id)
-                for label_id in label_ids
-            ]
+            fb_labels = [create_fb_label(fbb, list(self.labels.keys())[label_id], label_id) for label_id in label_ids]
             fb_labels_with_category = self.create_label_categories(fb_labels, str(item))
-            yield bytes(self.create_image(item['image'], fb_labels_with_category))
+            yield bytes(self.create_image(fbb, item['image'], fb_labels_with_category))
 
     def run(self) -> None:
         with open(self.datumaro_path, 'r') as f:
             # TODO: this loads the entire json file into memory, bad
             data = json.load(f)
             self.create_labels(data['categories']['label']['labels'])
-            self.send_images(self.iterate_json(data))
-
-    def send_images(self, images: List[Image.Image]) -> None:
-        grpc_channel = get_gRPC_channel()
-        image_service = ImageService.ImageServiceStub(grpc_channel)
-        for image in images:
-            print(f"Sending image: {len(image) / 1000} kB")
-            image_service.TransferImage(bytes(image))
-            break
+            gen = self.iterate_json(data)
+            grpc_channel = get_gRPC_channel()
+            image_stub = ImageService.ImageServiceStub(grpc_channel)
+            try:
+                image_stub.TransferImage(gen)
+            except Exception as e:
+                print(e)
 
 
 if __name__ == '__main__':
