7fe17a142d887244556bff2c41b8523b46744f4d
mmeijerdfki <marc.meijer@dfki.de>
2024-03-27 17:57:48 +0100

    improved schema path finding

--- examples/python/gRPC/util/fb_to_dict.py
+++ examples/python/gRPC/util/fb_to_dict.py
@@ -5,7 +5,8 @@ import tempfile as tf
 from pathlib import Path
 from typing import Dict, Final
 
-SCHEMA_FOLDER: Final[str] = "/seerep/src/seerep_msgs/fbs/"
+ROS_SCHEMA_PKG: Final[str] = "seerep_msgs"
+ROS_SCHEMA_SUBDIR: Final[str] = "fbs"
 
 
 class SchemaFileNames:
@@ -73,9 +74,41 @@ class SchemaFileNames:
     VECTOR3_STAMPED: Final[str] = "vector3_stamped.fbs"
 
 
+def catkin_find_schema_dir(ros_pkg_name: str, sub_dir: str) -> Path:
+    """
+    Tries to find the schema directory on the system using `catkin locate`.
+
+    Args:
+        ros_pkg_name: The name of the ros package containing the relevant schema files
+        sub_dir: The name of the subdir of the package dir containing the relevant schema files
+
+    Returns:
+        The schema directory on the system if found.
+
+    Raises:
+        FileNotFoundError: If the path on the system is not present.
+        ChildProcessError: If `catkin locate` returns something on stderr or failed otherwise
+    """
+
+    catkin_proc = sp.Popen(["catkin", "locate", ros_pkg_name], encoding="utf-8", stdout=sp.PIPE, stderr=sp.PIPE)
+    catkin_out, catkin_err = catkin_proc.communicate()
+
+    if catkin_proc.returncode != 0 or catkin_err != "":
+        raise ChildProcessError(f"Catkin call errored! {catkin_err}")
+
+    # splitlines needs to be used otherwise catkin_out contains \n at the end
+    fbs_path: Path = Path(catkin_out.splitlines()[0]) / sub_dir
+
+    if not fbs_path.is_dir():
+        raise FileNotFoundError(f"The path for seerep flatbuffers schema files is not present! Should be {fbs_path}")
+
+    return fbs_path
+
+
 def fb_flatc_dict(fb_obj: bytearray, schema_file_name: str) -> Dict:
     """
     Converts a binary flatbuffers object to a python dictionary using it's IDL file.
+
     This implementation uses temporary files in /tmp for conversion.
 
     Args:
@@ -84,8 +117,12 @@ def fb_flatc_dict(fb_obj: bytearray, schema_file_name: str) -> Dict:
 
     Returns:
         A python dictionary containing the objects attribute information.
+
+    Raises:
+        FileNotFoundError: If the schema file on the system couldn't be found.
+        ChildProcessError: If something went wrong using the flatc subcommand.
     """
-    schema_path = Path(SCHEMA_FOLDER + schema_file_name).absolute()
+    schema_path = catkin_find_schema_dir(ROS_SCHEMA_PKG, ROS_SCHEMA_SUBDIR) / schema_file_name
 
     if not schema_path.is_file():
         raise FileNotFoundError(f"The schema file at {schema_path} does not exist!")
