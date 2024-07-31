50164c3e5666423c3db04b9813cdee3affd9a95e
Julian Arkenau <julian.arkenau@dfki.de>
2024-07-31 12:20:25 +0200

    tests: update flatbuffer image integrations tests to new query functions

--- tests/python/gRPC/images/test_gRPC_fb_sendAndQueryImages.py
+++ tests/python/gRPC/images/test_gRPC_fb_sendAndQueryImages.py
@@ -1,39 +1,40 @@
 # test file for
-#   gRPC_fb_addImage.py
+#   gRPC_fb_sendImages.py
 #   gRPC_fb_queryImages.py
 
-from gRPC.images import gRPC_fb_addImage as add_image
+
+import flatbuffers
+from gRPC.images import gRPC_fb_queryImages, gRPC_fb_sendImages
 from seerep.util.fb_to_dict import SchemaFileNames, fb_flatc_dict
 
 
-def test_gRPC_fb_addAndQueryImages(grpc_channel, project_setup, num_images=10):
+def test_gRPC_fb_sendAndQueryImages(grpc_channel, project_setup, num_images=30):
     _, proj_uuid = project_setup
 
-    camera_intrinsics_uuid = add_image.send_cameraintrinsics(
+    camera_intrinsics_uuid = gRPC_fb_sendImages.send_cameraintrinsics(
         grpc_channel, proj_uuid
     )
 
-    # test for image uuid, general label and the general label confidence
-    sent_images = add_image.add_images(
+    sent_images = gRPC_fb_sendImages.send_images(
         grpc_channel,
         proj_uuid,
         camera_intrinsics_uuid,
-        add_image.generate_image_storage(num_images),
+        gRPC_fb_sendImages.generate_image_ressources(num_images),
     )
 
-    queried_images = add_image.get_image(grpc_channel, proj_uuid)
+    queried_images = gRPC_fb_queryImages.query_images_raw(
+        flatbuffers.Builder(), grpc_channel, proj_uuid
+    )
 
     assert queried_images is not None
 
-    sent_images = sorted(
-        [fb_flatc_dict(img, SchemaFileNames.IMAGE) for img in sent_images],
-        key=lambda img: img["header"]["uuid_msgs"],
-    )
+    sent_images = [
+        fb_flatc_dict(img, SchemaFileNames.IMAGE) for img in sent_images
+    ]
 
-    queried_images = sorted(
-        [fb_flatc_dict(img, SchemaFileNames.IMAGE) for img in queried_images],
-        key=lambda img: img["header"]["uuid_msgs"],
-    )
+    queried_images = [
+        fb_flatc_dict(img, SchemaFileNames.IMAGE) for img in queried_images
+    ]
 
     assert len(queried_images) == len(sent_images)
 
