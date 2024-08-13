120dea49a575c5bc548cd2f9aed90f5244cbecaf
Julian Arkenau <julian.arkenau@dfki.de>
2024-08-13 14:36:44 +0200

    update rostopic dumper to new hdf5 interface

--- seerep_ros/seerep_ros_comm/include/seerep_ros_comm/rostopic_dumper.h
+++ seerep_ros/seerep_ros_comm/include/seerep_ros_comm/rostopic_dumper.h
@@ -1,59 +1,83 @@
-#ifndef SEEREP_ROS_ROSTOPIC_DUMPER_H_
-#define SEEREP_ROS_ROSTOPIC_DUMPER_H_
+#ifndef HDF5_NODE_H
+#define HDF5_NODE_H
 
 #include <ros/master.h>
 #include <ros/ros.h>
-#include <seerep_hdf5_core/hdf5_core_general.h>
-#include <seerep_hdf5_core/hdf5_core_image.h>
-#include <seerep_hdf5_pb/hdf5_pb_image.h>
-#include <seerep_hdf5_pb/hdf5_pb_pointcloud.h>
-#include <seerep_hdf5_pb/hdf5_pb_tf.h>
-#include <seerep_ros_conversions_pb/conversions.h>
-#include <tf2_msgs/TFMessage.h>
+#include <seerep_hdf5_ros/hdf5_ros.h>
 
-#include <boost/functional/hash.hpp>
 #include <boost/lexical_cast.hpp>
-#include <boost/uuid/uuid.hpp>
 #include <boost/uuid/uuid_generators.hpp>
-#include <boost/uuid/uuid_io.hpp>
-#include <functional>
+#include <chrono>
 #include <optional>
+#include <thread>
 
-#include "types.h"
-
-namespace seerep_ros_comm
+namespace seerep_ros_examples
 {
-class DumpSensorMsgs
+
+std::string getCameraInfoTopic(const std::string& imageTopic);
+bool topicAvailable(const std::string& topic, ros::master::TopicInfo& info);
+
+class Hdf5Node
 {
 public:
-  DumpSensorMsgs(std::string hdf5FilePath, std::string project_frame_id,
-                 std::string project_name);
+  Hdf5Node() = delete;
 
-  std::optional<ros::Subscriber> getSubscriber(const std::string& message_type,
-                                               const std::string& topic);
+  Hdf5Node(const ros::NodeHandle& nh, const ros::NodeHandle& pnh);
 
-  void dump(const std_msgs::Header::ConstPtr& msg) const;
-
-  void dump(const sensor_msgs::PointCloud2::ConstPtr& msg) const;
-
-  void dump(const sensor_msgs::Image::ConstPtr& msg) const;
-
-  void dump(const geometry_msgs::Point::ConstPtr& msg) const;
-
-  void dump(const geometry_msgs::Quaternion::ConstPtr& msg) const;
-
-  void dump(const tf2_msgs::TFMessage::ConstPtr& msg) const;
+  void subscribe();
 
 private:
-  std::vector<seerep_core_msgs::LabelCategory> m_labelsCategory;
+  ros::NodeHandle nh_;
+  ros::NodeHandle pnh_;
 
-  std::shared_ptr<seerep_hdf5_pb::Hdf5PbTf> m_ioTf;
-  std::shared_ptr<seerep_hdf5_pb::Hdf5PbPointCloud> m_ioPointCloud;
-  std::shared_ptr<seerep_hdf5_pb::Hdf5PbImage> m_ioImage;
-  std::shared_ptr<seerep_hdf5_core::Hdf5CoreImage> m_ioImageCore;
-  ros::NodeHandle nh;
+  /* store the topics to dump */
+  std::vector<std::string> topics_;
+
+  /* keeps the subsribers alive outside of their initial function scope */
+  std::map<std::string, ros::Subscriber> subscribers_;
+
+  /*
+   * Current workaround is to store the first camera info message and use it
+   * for all following images. If the camera info is  subject to change, the
+   * camera and camera info topic should be time synchronized.
+   * This could be achieved by using message filters.
+   * http://wiki.ros.org/message_filters
+   */
+  std::string cameraInfoUuid_;
+
+  /* composition to the seerep_hdf5_ros interface */
+  std::unique_ptr<seerep_hdf5_ros::Hdf5Ros> hdf5Ros_;
+
+  /* @brief Get a parameter from the parameter server.
+   *
+   * @tparam T Type of the parameter.
+   * @param name Nname of the parameter.
+   * @return Vvalue of the parameter.
+   * @throws std::runtime_error if the parameter is missing.
+   */
+  template <typename T>
+  T getParameter(const std::string& name);
+
+  /*
+    Callbacks for the subscribers, some require additional logic, thus it is
+    not possible to unify them with a template.
+  */
+  void callback(const sensor_msgs::Image::ConstPtr& msg);
+  void callback(const sensor_msgs::PointCloud2::ConstPtr& msg);
+  void callback(const tf2_msgs::TFMessage::ConstPtr& msg);
 };
 
-}  // namespace seerep_ros_comm
+template <typename T>
+T Hdf5Node::getParameter(const std::string& name)
+{
+  T value;
+  if (!pnh_.getParam(name, value))
+  {
+    throw std::runtime_error("Paramter '" + name + "' is missing");
+  }
+  return value;
+}
 
-#endif  // SEEREP_ROS_ROSTOPIC_DUMPER_H_
+} /* Namespace seerep_ros_examples */
+
+#endif /* HDF5_NODE_H_ */
